6fdc11e72acbf0d64dca6e05842553abbd57535b
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-08-02 16:20:34 +1000

    core: Add DB insertion functions for all DB records

---
+++ cardano-explorer-core/src/Explorer/Core/DB/Insert.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Explorer.Core.DB.Insert
+  ( insertBlock
+  , insertTx
+  , insertTxin
+  , insertTxout
+
+  -- Export mainly for testing.
+  , insertByReturnKeyE
+  ) where
+
+
+import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Trans.Reader (ReaderT)
+
+import           Database.Persist.Class (AtLeastOneUniqueKey, Key, PersistEntityBackend,
+                    getByValue, insert)
+import           Database.Persist.Sql (SqlBackend)
+import           Database.Persist.Types (entityKey)
+
+import           Explorer.Core.DB.Schema
+
+
+insertBlock :: MonadIO m => Block -> ReaderT SqlBackend m (Either BlockId BlockId)
+insertBlock = insertByReturnKeyE
+
+insertTx :: MonadIO m => Tx -> ReaderT SqlBackend m (Either TxId TxId)
+insertTx = insertByReturnKeyE
+
+insertTxin :: MonadIO m => Txin -> ReaderT SqlBackend m (Either TxinId TxinId)
+insertTxin = insertByReturnKeyE
+
+insertTxout :: MonadIO m => Txout -> ReaderT SqlBackend m (Either TxoutId TxoutId)
+insertTxout = insertByReturnKeyE
+
+-- | Insert a record (with a Unique constraing), and return 'Right key' if the record
+-- is inserted and 'Left key' if the record already exists in the DB.
+insertByReturnKeyE
+    :: ( AtLeastOneUniqueKey record
+       , MonadIO m
+       , PersistEntityBackend record ~ SqlBackend
+       )
+    => record -> ReaderT SqlBackend m (Either (Key record) (Key record))
+insertByReturnKeyE value = do
+  res <- getByValue value
+  case res of
+    Nothing -> Right <$> insert value
+    Just r -> pure $ Left (entityKey r)
