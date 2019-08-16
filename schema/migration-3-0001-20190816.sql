a9172726d440e90e6e959aa51162233af6957b3e
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-08-16 18:10:33 +1000

    Schema udpates
    * Update DOMAIN types because addresses are 28 bytes whereas the other
      hashes are 32 bytes.
    * Drop the 'txOutId' field of the 'TxIn' table.
    * Use a 'LEFT OUTER JOIN' to create the 'utxo' view table.

--- schema/migration-3-0001-20190816.sql
+++ schema/migration-3-0001-20190816.sql
@@ -8,7 +8,7 @@ BEGIN
   IF next_version = 1 THEN
     -- TODO: Is there a way to avoid 'NOT IN' using an 'OUTER JOIN'?
     -- Does the 'OUTER JOIN' actually perform better?
-    EXECUTE 'CREATe VIEW utxo AS SELECT * FROM tx_out WHERE tx_out.id NOT IN (SELECT tx_out_id FROM tx_in)' ;
+    EXECUTE 'CREATe VIEW utxo AS SELECT tx_out.* FROM tx_out LEFT OUTER JOIN tx_in ON tx_out.tx_id = tx_in.tx_id AND tx_out.index = tx_in.index WHERE tx_in.tx_id IS NOT NULL' ;
 
     UPDATE schema_version SET stage_three = 1;
     RAISE NOTICE 'DB has been migrated to stage_three version %', next_version ;
