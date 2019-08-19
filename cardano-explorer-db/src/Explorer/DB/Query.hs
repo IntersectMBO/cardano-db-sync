be7c9be3120f81a3d4f9c978e75692ee30a57688
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-08-19 17:54:08 +1000

    db: Rejig queryLatestBlocks

--- cardano-explorer-db/src/Explorer/DB/Query.hs
+++ cardano-explorer-db/src/Explorer/DB/Query.hs
@@ -4,8 +4,8 @@
 module Explorer.DB.Query
   ( queryBlockCount
   , queryBlockId
+  , queryLatestBlock
   , queryTxId
-  , queryLatestBlocks
   , queryTxOutValue
   , querySelectCount
   ) where
@@ -17,7 +17,8 @@ import           Control.Monad.Trans.Reader (ReaderT)
 import           Data.ByteString.Char8 (ByteString)
 import           Data.Word (Word16, Word64)
 
-import           Database.Esqueleto (From, InnerJoin (..), SqlQuery, (^.), (==.), (&&.),
+import           Database.Esqueleto (From, InnerJoin (..), SqlQuery,
+                    (^.), (==.), (&&.),
                     countRows, desc, entityKey, from, limit, on, orderBy, select,
                     unValue, val, where_)
 import           Database.Persist.Sql (SqlBackend, entityVal)
@@ -28,39 +29,35 @@ import           Explorer.DB.Schema
 
 
 
--- | Get the last N blocks.
--- This assumes that the block are inserted into the datebase from oldest to
--- newest which is not exlicitly enforced, but should arise automatically
--- from the way blocks are retrieved and inserted into the database.
-queryLatestBlocks :: MonadIO m => Int -> ReaderT SqlBackend m [Block]
-queryLatestBlocks limitCount = do
-  eblks <- select $ from $ \ blk -> do
-              orderBy [desc (blk ^. BlockId)]
-              limit $ fromIntegral limitCount
-              pure blk
-  pure $ map entityVal eblks
-
-
 -- | Count the number of blocks in the Block table.
 queryBlockCount :: (MonadIO m, From Block) => ReaderT SqlBackend m Word
 queryBlockCount = do
-  res <- select $ from $ \ (_ :: Block ) ->
+  res <- select . from $ \ (_ :: Block) ->
             pure countRows
   pure $ maybe 0 unValue (listToMaybe res)
 
 -- | Get the 'BlockId' associated with the given hash.
 queryBlockId :: MonadIO m => ByteString -> ReaderT SqlBackend m (Maybe BlockId)
 queryBlockId hash = do
-  res <- select $ from $ \ blk -> do
+  res <- select . from $ \ blk -> do
             where_ (blk ^. BlockHash ==. val hash)
             pure blk
   pure $ fmap entityKey (listToMaybe res)
 
+-- | Get the latest block. Needed so that the explorer can continue syncing
+-- from its current latest blocks.
+queryLatestBlock :: MonadIO m => ReaderT SqlBackend m (Maybe Block)
+queryLatestBlock = do
+  res <- select . from $ \ blk -> do
+            orderBy [desc (blk ^. BlockBlockNo)]
+            limit 1
+            pure blk
+  pure $ fmap entityVal (listToMaybe res)
 
 -- | Get the 'TxId' associated with the given hash.
 queryTxId :: MonadIO m => ByteString -> ReaderT SqlBackend m (Maybe TxId)
 queryTxId hash = do
-  res <- select $ from $ \ tx -> do
+  res <- select . from $ \ tx -> do
             where_ (tx ^. TxHash ==. val hash)
             pure tx
   pure $ fmap entityKey (listToMaybe res)
@@ -68,7 +65,7 @@ queryTxId hash = do
 -- | Give a tx hash, and an index for the specific outut, return the TxOut value.
 queryTxOutValue :: MonadIO m => (ByteString, Word16) -> ReaderT SqlBackend m (Maybe Word64)
 queryTxOutValue (hash, index) = do
-  res <- select $ from $ \ (tx `InnerJoin` txo) -> do
+  res <- select . from $ \ (tx `InnerJoin` txo) -> do
             on (tx ^. TxId ==. txo ^. TxOutTxId)
             where_ (txo ^. TxOutIndex ==. val index
                     &&. tx ^. TxHash ==. val hash
@@ -79,7 +76,7 @@ queryTxOutValue (hash, index) = do
 -- | Count the number of rows that match the select with the supplied predicate.
 querySelectCount :: (MonadIO m, From table) => (table -> SqlQuery ()) -> ReaderT SqlBackend m Word
 querySelectCount predicate = do
-  xs <- select $ from $ \x -> do
+  xs <- select . from $ \x -> do
             predicate x
             pure countRows
   pure $ maybe 0 unValue (listToMaybe xs)
