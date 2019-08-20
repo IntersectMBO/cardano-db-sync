e7b2eb5d4cbdf7ddad82db970b2c68d968d244c1
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-08-20 12:18:35 +1000

    Update the schema

--- schema/migration-1-0001-20190730.sql
+++ schema/migration-1-0001-20190730.sql
@@ -9,9 +9,18 @@ BEGIN
   SELECT stage_one + 1 INTO next_version FROM "schema_version";
   IF next_version = 1 THEN
     EXECUTE 'CREATE DOMAIN lovelace AS bigint CHECK (VALUE >= 0 AND VALUE <= 45000000000000000);';
-    EXECUTE 'CREATE DOMAIN hashtype AS bytea CHECK (octet_length (VALUE) = 32);';
     EXECUTE 'CREATE DOMAIN txindex AS smallint CHECK (VALUE >= 0 AND VALUE < 1024);';
     EXECUTE 'CREATE DOMAIN uinteger AS integer CHECK (VALUE >= 0);';
+
+    -- Blocks, transactions and merkel roots use a 32 byte hash.
+    EXECUTE 'CREATE DOMAIN hash32type AS bytea CHECK (octet_length (VALUE) = 32);';
+
+    -- Addresses use a 28 byte hash.
+    EXECUTE 'CREATE DOMAIN hash28type AS bytea CHECK (octet_length (VALUE) = 28);';
+
+    -- Drop this view if because this migration changes types of columns the view
+    -- referred to. We can recreate the view later in the migration schedule.
+    EXECUTE 'DROP VIEW IF EXISTS utxo;';
 
     UPDATE "schema_version" SET stage_one = 1;
     RAISE NOTICE 'DB has been migrated to stage_one version %', next_version;
--- schema/migration-3-0001-20190816.sql
+++ schema/migration-3-0001-20190816.sql
@@ -8,7 +8,7 @@ BEGIN
   IF next_version = 1 THEN
     -- TODO: Is there a way to avoid 'NOT IN' using an 'OUTER JOIN'?
     -- Does the 'OUTER JOIN' actually perform better?
-    EXECUTE 'CREATe VIEW utxo AS SELECT tx_out.* FROM tx_out LEFT OUTER JOIN tx_in ON tx_out.tx_id = tx_in.tx_id AND tx_out.index = tx_in.index WHERE tx_in.tx_id IS NOT NULL' ;
+    EXECUTE 'CREATe VIEW utxo AS SELECT tx_out.* FROM tx_out LEFT OUTER JOIN tx_in ON tx_out.tx_id = tx_in.tx_out_id AND tx_out.index = tx_in.tx_out_index WHERE tx_in.tx_in_id IS NULL' ;
 
     UPDATE schema_version SET stage_three = 1;
     RAISE NOTICE 'DB has been migrated to stage_three version %', next_version ;
