44db88fb0830042f8f01b024016dbd9ce1c90187
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-08-28 15:31:49 +1000

    db: Refactor tests to remove duplication

--- cardano-explorer-db/test/Test/IO/Explorer/DB/TotalSupply.hs
+++ cardano-explorer-db/test/Test/IO/Explorer/DB/TotalSupply.hs
@@ -4,23 +4,12 @@ module Test.IO.Explorer.DB.TotalSupply
   ( tests
   ) where
 
-import           Control.Monad (unless)
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Control.Monad.Trans.Reader (ReaderT)
-
-import           Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as BS
-import           Data.Word (Word64)
-
-import           Database.Persist.Sql (SqlBackend, deleteCascade, selectKeysList, unSqlBackendKey)
-
 import           Explorer.DB
 
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.HUnit (testCase)
--- import           Test.HUnit.Base (assertBool)
 
-import           Text.Printf (printf)
+import           Test.IO.Explorer.DB.Util
 
 tests :: TestTree
 tests =
@@ -51,47 +40,3 @@ initialSupplyTest =
     _ <- insertTxOut $ TxOut tx1Id 0 (mkAddressHash bid1 tx1Id) 500000000
     supply1 <- queryTotalSupply
     assertBool ("Total supply should be < " ++ show supply0) (supply1 < supply0)
-
-
--- Could use 'assertBool' from HUNit, but need 'MonadIO'.
-assertBool :: MonadIO m => String -> Bool -> m ()
-assertBool msg bool =
-  liftIO $ unless bool (error msg)
-
-mkBlock :: Word64 -> Block
-mkBlock blk =
-  Block (mkBlockHash blk) Nothing 0 Nothing Nothing 42
-
-mkTxs :: BlockId -> Word -> [Tx]
-mkTxs blkId count =
-    take (fromIntegral count) $ map create [ 0 .. ]
-  where
-    create w = Tx (mkTxHash blkId w) blkId 1
-
-mkTxOut :: BlockId -> TxId -> TxOut
-mkTxOut blkId txId =
-  TxOut txId 0 (mkAddressHash blkId txId) 1000000000
-
-mkBlockHash :: Word64 -> ByteString
-mkBlockHash blk =
-  BS.pack (take 32 $ printf "block #%d" blk ++ replicate 32 ' ')
-
-mkTxHash :: BlockId -> Word64 -> ByteString
-mkTxHash blk tx =
-  BS.pack (take 32 $ printf "block #%d, tx #%d" (unBlockId blk) tx ++ replicate 32 ' ')
-
-mkAddressHash :: BlockId -> TxId -> ByteString
-mkAddressHash blkId txId =
-  BS.pack (take 28 $ printf "tx out #%d, tx #%d" (unBlockId blkId) (unTxId txId) ++ replicate 28 ' ')
-
--- Surely there is a better way!
-unBlockId :: BlockId -> Word64
-unBlockId = fromIntegral . unSqlBackendKey . unBlockKey
-
-unTxId :: TxId -> Word64
-unTxId = fromIntegral . unSqlBackendKey . unTxKey
-
-deleteAllBlocksCascade :: MonadIO m => ReaderT SqlBackend m ()
-deleteAllBlocksCascade = do
-  (keys :: [BlockId]) <- selectKeysList [] []
-  mapM_ deleteCascade keys
---
+++ cardano-explorer-db/test/Test/IO/Explorer/DB/Util.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Test.IO.Explorer.DB.Util
+  ( assertBool
+  , deleteAllBlocksCascade
+  , mkAddressHash
+  , mkBlock
+  , mkBlockHash
+  , mkMerkelRoot
+  , mkTxHash
+  , mkTxs
+  , mkTxOut
+  , unBlockId
+  , unTxId
+  ) where
+
+import           Control.Monad (unless)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           Control.Monad.Trans.Reader (ReaderT)
+
+import           Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import           Data.Word (Word64)
+
+import           Database.Persist.Sql (SqlBackend, deleteCascade, selectKeysList, unSqlBackendKey)
+
+import           Explorer.DB
+
+import           Text.Printf (printf)
+
+
+assertBool :: MonadIO m => String -> Bool -> m ()
+assertBool msg bool =
+  liftIO $ unless bool (error msg)
+
+deleteAllBlocksCascade :: MonadIO m => ReaderT SqlBackend m ()
+deleteAllBlocksCascade = do
+  (keys :: [BlockId]) <- selectKeysList [] []
+  mapM_ deleteCascade keys
+
+mkAddressHash :: BlockId -> TxId -> ByteString
+mkAddressHash blkId txId =
+  BS.pack (take 28 $ printf "tx out #%d, tx #%d" (unBlockId blkId) (unTxId txId) ++ replicate 28 ' ')
+
+mkBlock :: Word64 -> Block
+mkBlock blk =
+  Block (mkBlockHash blk) Nothing 0 Nothing Nothing 42
+
+mkBlockHash :: Word64 -> ByteString
+mkBlockHash blkId =
+  BS.pack (take 32 $ printf "block #%d" blkId ++ replicate 32 ' ')
+
+mkMerkelRoot :: Word64 -> ByteString
+mkMerkelRoot blkId =
+  BS.pack (take 32 $ printf "merkel root #%d" blkId ++ replicate 32 ' ')
+
+mkTxHash :: BlockId -> Word64 -> ByteString
+mkTxHash blk tx =
+  BS.pack (take 32 $ printf "block #%d, tx #%d" (unBlockId blk) tx ++ replicate 32 ' ')
+
+mkTxs :: BlockId -> Word -> [Tx]
+mkTxs blkId count =
+    take (fromIntegral count) $ map create [ 0 .. ]
+  where
+    create w = Tx (mkTxHash blkId w) blkId 1
+
+mkTxOut :: BlockId -> TxId -> TxOut
+mkTxOut blkId txId =
+  TxOut txId 0 (mkAddressHash blkId txId) 1000000000
+
+unBlockId :: BlockId -> Word64
+unBlockId = fromIntegral . unSqlBackendKey . unBlockKey
+
+unTxId :: TxId -> Word64
+unTxId = fromIntegral . unSqlBackendKey . unTxKey
