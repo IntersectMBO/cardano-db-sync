0208b4853c43cf4470b0df4b8d625923b26dbe16
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-08-30 14:43:35 +1000

    db: Add round trop to/from JSON test for Ada

--- cardano-explorer-db/src/Explorer/DB/Types.hs
+++ cardano-explorer-db/src/Explorer/DB/Types.hs
@@ -7,22 +7,32 @@ module Explorer.DB.Types
   ( Ada (..)
   , lovelaceToAda
   , renderAda
+  , scientificToAda
   , word64ToAda
   ) where
 
 
 import           Data.Aeson.Encoding (unsafeToEncoding)
-import           Data.Aeson.Types (ToJSON (..))
+import           Data.Aeson.Types (FromJSON (..), ToJSON (..), Value (..), (.:))
+import qualified Data.Aeson.Types as Aeson
 import qualified Data.ByteString.Builder as BS (string8)
 import           Data.Fixed (Micro, showFixed)
+import           Data.Scientific (Scientific)
 import           Data.Text (Text)
 import qualified Data.Text as Text
 import           Data.Word (Word64)
+
 import           GHC.Generics (Generic)
 
 newtype Ada = Ada
   { unAda :: Micro
   } deriving (Eq, Ord, Generic)
+
+instance FromJSON Ada where
+  parseJSON val =
+    case val of
+      Object obj -> scientificToAda <$> obj .: "unAda"
+      _other -> Aeson.typeMismatch "Ada" val
 
 instance ToJSON Ada where
     --toJSON (Ada ada) = Data.Aeson.Types.Number $ fromRational $ toRational ada
@@ -39,9 +49,14 @@ lovelaceToAda :: Micro -> Ada
 lovelaceToAda ll =
   Ada (ll / 1000000)
 
+renderAda :: Ada -> Text
+renderAda (Ada a) = Text.pack (show a)
+
+scientificToAda :: Scientific -> Ada
+scientificToAda s =
+  word64ToAda $ floor (s * 1000000)
+
 word64ToAda :: Word64 -> Ada
 word64ToAda w =
   Ada (fromIntegral w / 1000000)
 
-renderAda :: Ada -> Text
-renderAda (Ada a) = Text.pack (show a)
--- cardano-explorer-db/test/test.hs
+++ cardano-explorer-db/test/test.hs
@@ -1,9 +1,11 @@
 import           Hedgehog.Main (defaultMain)
 
 import qualified Test.Property.Explorer.DB.Migration
+import qualified Test.Property.Explorer.DB.Types
 
 main :: IO ()
 main =
   defaultMain
     [ Test.Property.Explorer.DB.Migration.tests
+    , Test.Property.Explorer.DB.Types.tests
     ]
