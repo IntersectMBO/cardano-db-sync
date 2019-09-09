input-output-hk/cardano-explorer

Regenerated nix packages

nix/.stack.nix/contra-tracer.nix
@@ -24,8 +24,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/iohk-monitoring-framework";
-      rev = "4ad81798402ce05655b37b31b0b67f444a2f0b91";
-      sha256 = "130k6msxrxr0dai0gk2r9ca2bjpwqpklk7rxqf1yrn2plivkqqlm";
+      rev = "c85d57a95d919336afea7e0eff00e834bd6b4b58";
+      sha256 = "1ia9z6bgavf2ng49d8f9anlsy9jln8ab20a92spkr2rd17c51qhv";
       });
     postUnpack = "sourceRoot+=/contra-tracer; echo source root reset to \$sourceRoot";
     }

nix/.stack.nix/iohk-monitoring.nix
@@ -156,8 +156,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/iohk-monitoring-framework";
-      rev = "4ad81798402ce05655b37b31b0b67f444a2f0b91";
-      sha256 = "130k6msxrxr0dai0gk2r9ca2bjpwqpklk7rxqf1yrn2plivkqqlm";
+      rev = "c85d57a95d919336afea7e0eff00e834bd6b4b58";
+      sha256 = "1ia9z6bgavf2ng49d8f9anlsy9jln8ab20a92spkr2rd17c51qhv";
       });
     postUnpack = "sourceRoot+=/iohk-monitoring; echo source root reset to \$sourceRoot";
     }

IntersectMBO/cardano-db-sync

nix/.stack.nix/contra-tracer.nix
@@ -24,8 +24,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/iohk-monitoring-framework";
-      rev = "4ad81798402ce05655b37b31b0b67f444a2f0b91";
-      sha256 = "130k6msxrxr0dai0gk2r9ca2bjpwqpklk7rxqf1yrn2plivkqqlm";
+      rev = "e22be186e0b1018260ed7c5166871f82a869b94d";
+      sha256 = "1nihpxmkhlhx3sfnwxprhfa0f70rc97zkvhd66hl3wcifgjp40aj";
       });
     postUnpack = "sourceRoot+=/contra-tracer; echo source root reset to \$sourceRoot";
     }

nix/.stack.nix/iohk-monitoring.nix
@@ -156,8 +156,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/iohk-monitoring-framework";
-      rev = "4ad81798402ce05655b37b31b0b67f444a2f0b91";
-      sha256 = "130k6msxrxr0dai0gk2r9ca2bjpwqpklk7rxqf1yrn2plivkqqlm";
+      rev = "e22be186e0b1018260ed7c5166871f82a869b94d";
+      sha256 = "1nihpxmkhlhx3sfnwxprhfa0f70rc97zkvhd66hl3wcifgjp40aj";
       });
     postUnpack = "sourceRoot+=/iohk-monitoring; echo source root reset to \$sourceRoot";
     }