b035a6a951434ae9459add80346de18f73767d86
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-09-13 07:40:45 +1000

    db: Fix/improve the way VIEWs are handled in migrations
    DB schema migrations can fail if they affect a table on which a
    VIEW is defined. Therefore, delete all VIEWs at the start of the
    migration and recreate them after.

---
+++ schema/migration-1-0002-20190912.sql
@@ -0,0 +1,23 @@
+-- Drop all VIEWs linked to the 'cexplorer' table.
+
+-- Many schema migration changes will fail if they operate on a table that is part of a VIEW.
+-- So we drop all VIEWs here and recreate them later.
+
+CREATE FUNCTION drop_cexplorer_views () RETURNS void language plpgsql as $$
+
+DECLARE vname text;
+
+BEGIN
+  for vname in
+      select table_name
+	    from information_schema.views
+        where table_catalog = 'cexplorer' and table_schema = 'public'
+    loop
+      execute format ('drop view %s ;', vname) ;
+      raise notice 'Dropping view : %', vname ;
+    end loop ;
+end $$ ;
+
+SELECT drop_cexplorer_views () ;
+
+DROP FUNCTION drop_cexplorer_views () ;
--- schema/migration-3-0001-20190816.sql
+++ schema/migration-3-0001-20190816.sql
@@ -1,21 +1,10 @@
--- Persistent generated migration.
+-- Create all the VIEWs.
 
-CREATE FUNCTION migrate() RETURNS void AS $$
-DECLARE
-  next_version int ;
-BEGIN
-  SELECT stage_three + 1 INTO next_version FROM schema_version ;
-  IF next_version = 1 THEN
-    -- TODO: Is there a way to avoid 'NOT IN' using an 'OUTER JOIN'?
-    -- Does the 'OUTER JOIN' actually perform better?
-    EXECUTE 'CREATe VIEW utxo AS SELECT tx_out.* FROM tx_out LEFT OUTER JOIN tx_in ON tx_out.tx_id = tx_in.tx_out_id AND tx_out.index = tx_in.tx_out_index WHERE tx_in.tx_in_id IS NULL' ;
+-- The VIEWs are always created because all VIEWs are deleted at the start ot the
+-- migration process.
 
-    UPDATE schema_version SET stage_three = 1;
-    RAISE NOTICE 'DB has been migrated to stage_three version %', next_version ;
-  END IF ;
-END ;
-$$ LANGUAGE plpgsql ;
-
-SELECT migrate() ;
-
-DROP FUNCTION migrate() ;
+-- The standard utxo view which shows all unspent transaction outputs.
+create view utxo as select tx_out.*
+  from tx_out left outer join tx_in
+	on tx_out.tx_id = tx_in.tx_out_id and tx_out.index = tx_in.tx_out_index
+  where tx_in.tx_in_id is null ;
