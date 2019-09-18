f73b1d808aff4c263ee1c41b9756feb1df9bc78c
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-09-18 12:57:17 +1000

    db: Add a migration to populate the epoch_no field
    Co-authored-by: Sam Jeston <sam.jeston@iohk.io>

---
+++ schema/migration-2-0002-20190918.sql
@@ -0,0 +1,33 @@
+-- Persistent generated migration.
+
+CREATE FUNCTION migrate() RETURNS void AS $$
+DECLARE
+  next_version int ;
+BEGIN
+  SELECT stage_two + 1 INTO next_version FROM schema_version ;
+  IF next_version = 2 THEN
+    EXECUTE 'ALTER TABLE "block" ADD COLUMN "epoch_no" uinteger NULL' ;
+
+    -- Hand written SQL to insert the epoch_no field.
+    -- Update all non-EBBs.
+	UPDATE block SET epoch_no = div (slot_no, 21600) WHERE slot_no IS NOT NULL ;
+
+	-- Update EBBs.
+    UPDATE block SET epoch_no =
+      CASE WHEN previous_block.epoch_no IS NULL
+        THEN 0
+        ELSE previous_block.epoch_no + 1
+        END
+      FROM block AS previous_block
+      WHERE block.previous = previous_block.id AND block.epoch_no IS NULL ;
+    --
+
+    UPDATE schema_version SET stage_two = 2 ;
+    RAISE NOTICE 'DB has been migrated to stage_two version %', next_version ;
+  END IF ;
+END ;
+$$ LANGUAGE plpgsql ;
+
+SELECT migrate() ;
+
+DROP FUNCTION migrate() ;
