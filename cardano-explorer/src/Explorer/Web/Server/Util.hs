fddbc429fd1f6da0f05e9bb51f7b80c09c512421
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-18 18:31:28 +1100

    webapi: Implement /api/genesis/address/pages/total endpoint
    Closes: https://github.com/input-output-hk/cardano-explorer/issues/94

--- cardano-explorer/src/Explorer/Web/Server/Util.hs
+++ cardano-explorer/src/Explorer/Web/Server/Util.hs
@@ -3,11 +3,15 @@
 module Explorer.Web.Server.Util
   ( bsBase16Encode
   , decodeTextAddress
+  , defaultPageSize
+  , divRoundUp
   , k
+  , roundToBlockPage
   , runQuery
   , slotsPerEpoch
   , textBase16Decode
   , textShow
+  , toPageSize
   ) where
 
 import           Cardano.Chain.Common (Address, fromCBORTextAddress)
@@ -18,6 +22,7 @@ import           Control.Monad.Trans.Reader (ReaderT)
 import           Data.Bifunctor (first)
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Base16 as Base16
+import           Data.Maybe (fromMaybe)
 import           Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
@@ -40,9 +45,25 @@ decodeTextAddress txt =
   first (const . Internal $ "Unable to decode address " <> txt <> ".")
     $ fromCBORTextAddress txt
 
+
+defaultPageSize :: Word
+defaultPageSize = 10
+
+divRoundUp :: Integral a => a -> a -> a
+divRoundUp a b = (a + b - 1) `div` b
+
 -- TODO, get this from the config somehow
 k :: Word64
 k = 2160
+
+-- | A pure calculation of the page number.
+-- Get total pages from the blocks. And we want the page
+-- with the example, the page size 10,
+-- to start with 10 + 1 == 11, not with 10 since with
+-- 10 we'll have an empty page.
+-- Could also be `((blocksTotal - 1) `div` pageSizeInt) + 1`.
+roundToBlockPage :: Word -> Word
+roundToBlockPage blocksTotal = divRoundUp blocksTotal defaultPageSize
 
 runQuery :: MonadIO m => SqlBackend -> ReaderT SqlBackend IO a -> m a
 runQuery backend query =
@@ -59,3 +80,6 @@ textBase16Decode text = do
 
 textShow :: Show a => a -> Text
 textShow = Text.pack . show
+
+toPageSize :: Maybe Word -> Word
+toPageSize = fromMaybe defaultPageSize
