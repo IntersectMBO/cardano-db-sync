e66b14b74e9cb5bec3bc13ea2a36978b3244609c
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-24 07:49:41 +1100

    webapi: Improve esqueleto queries in Explorer.Web.Server.BlocksTxs

--- cardano-explorer/src/Explorer/Web/Server/BlocksTxs.hs
+++ cardano-explorer/src/Explorer/Web/Server/BlocksTxs.hs
@@ -12,25 +12,24 @@ import           Data.ByteString.Char8 (ByteString)
 
 import           Data.Int (Int64)
 import           Data.Maybe (fromMaybe)
-import           Data.Text (Text)
 import           Data.Time.Clock (UTCTime)
 import           Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import           Data.Text (Text)
 import           Data.Word (Word64)
 
-import           Database.Esqueleto (InnerJoin (..),
-                    (^.), (==.), (&&.), from, in_, limit, offset, on, select,
-                    unValue, val, valList, where_)
-import           Database.Persist.Sql (Entity (..), SqlBackend)
 
-import           Explorer.DB (BlockId, EntityField (..), TxId, TxOut (..),unValue2)
+import           Database.Esqueleto (InnerJoin (..), Value (..), (^.), (==.), (&&.),
+                    from, limit, in_, offset, on, select, val, valList, where_)
+import           Database.Persist.Sql (SqlBackend)
 
-import           Explorer.Web.ClientTypes (CAddress (..), CCoin, CHash (..),
-                    CTxBrief (..), CTxHash (..), mkCCoin)
+import           Explorer.DB (EntityField (..), TxId, unValue3)
+
+import           Explorer.Web.ClientTypes (CAddress (..), CHash (..), CTxBrief (..), CTxHash (..),
+                    mkCCoin)
 import           Explorer.Web.Error (ExplorerError (..))
 import           Explorer.Web.Server.Util (bsBase16Encode, runQuery, textBase16Decode)
 
 import           Servant (Handler)
-
 
 -- Example queries:
 --
@@ -55,50 +54,40 @@ blocksTxs backend (CHash blkHashTxt) mLimit mOffset =
 
 queryBlockByHash :: MonadIO m => ByteString -> Int64 -> Int64 -> ReaderT SqlBackend m (Either ExplorerError [CTxBrief])
 queryBlockByHash blkHash limitNum offsetNum  = do
-    res <- select . from $ \blk -> do
-            where_ (blk ^. BlockHash ==. val blkHash)
-            pure (blk ^. BlockId, blk ^. BlockTime)
-    case map unValue2 res of
-      [] -> pure $ Left (Internal "No block found")
-      ((blkid, utctime):_) -> Right <$> queryBlockTxs blkid utctime limitNum offsetNum
-
-queryBlockTxs :: MonadIO m => BlockId -> UTCTime -> Int64 -> Int64 -> ReaderT SqlBackend m [CTxBrief]
-queryBlockTxs blkId utctime limitNum offsetNum = do
-    res <- select . from $ \ (blk `InnerJoin` tx) -> do
+    res <- select . from $ \  (blk `InnerJoin` tx) -> do
             on (blk ^. BlockId ==. tx ^. TxBlock)
-            where_ (blk ^. BlockId ==. val blkId)
+            where_ (blk ^. BlockHash ==. val blkHash)
             limit limitNum
             offset offsetNum
-            pure (tx ^. TxId, tx ^. TxHash)
-    let txids = map (unValue. fst) res
-    outputs <- select . from $ \txout -> do
-                where_ (txout ^. TxOutTxId `in_` valList txids)
-                pure txout
-    mapM (queryCTxBrief utctime (map entityVal outputs) . unValue2) res
+            pure (tx ^. TxId, tx ^. TxHash, blk ^. BlockTime)
+    case map unValue3 res of
+      [] -> pure $ Left (Internal "No block found")
+      -- TODO: This can still do with some improvement.
+      xs -> Right <$> mapM (queryCTxBrief (map fst3 xs)) xs
+  where
+    fst3 :: (a, b, c) -> a
+    fst3 (a, _, _) = a
 
-queryCTxBrief :: MonadIO m => UTCTime -> [TxOut] -> (TxId, ByteString) -> ReaderT SqlBackend m CTxBrief
-queryCTxBrief utctime outputs (txid, txhash) = do
-  inputs <- queryTxInputs txid
-  pure $ CTxBrief
-          { ctbId = CTxHash . CHash $ bsBase16Encode txhash
-          , ctbTimeIssued = Just $ utcTimeToPOSIXSeconds utctime
-          , ctbInputs = map convertInput inputs
-          , ctbOutputs = map convertTxOut outputs
-          , ctbInputSum = mkCCoin . sum $ map (fromIntegral . snd) inputs
-          , ctbOutputSum = mkCCoin . sum $ map (fromIntegral . txOutValue) outputs
-          }
-
-
-queryTxInputs :: MonadIO m => TxId -> ReaderT SqlBackend m [(Text, Word64)]
-queryTxInputs txid = do
-  rows <- select . from $ \(txin `InnerJoin` txout) -> do
-    on (txin ^. TxInTxOutId ==. txout ^. TxOutTxId &&. txin ^. TxInTxOutIndex ==. txout ^. TxOutIndex)
-    where_ $ txin ^. TxInTxInId ==. val txid
-    pure (txout ^. TxOutAddress, txout ^. TxOutValue)
-  pure $ map unValue2 rows
-
-convertTxOut :: TxOut -> (CAddress, CCoin)
-convertTxOut txo = (CAddress $ txOutAddress txo, mkCCoin $ fromIntegral (txOutValue txo))
-
-convertInput :: (Text, Word64) -> Maybe (CAddress, CCoin)
-convertInput (addr, coin) = Just (CAddress $ addr, mkCCoin $ fromIntegral coin)
+queryCTxBrief :: MonadIO m => [TxId] -> (TxId, ByteString, UTCTime) -> ReaderT SqlBackend m CTxBrief
+queryCTxBrief txOutIds (txId, txhash, utctime) = do
+    inrows <- select . from $ \(txIn `InnerJoin` txOut) -> do
+                on (txIn ^. TxInTxOutId ==. txOut ^. TxOutTxId
+                    &&. txIn ^. TxInTxOutIndex ==. txOut ^. TxOutIndex)
+                where_ (txIn ^. TxInTxInId ==. val txId)
+                pure (txOut ^. TxOutAddress, txOut ^. TxOutValue)
+    let inputs = map convert inrows
+    outrows <- select . from $ \txOut -> do
+                  where_ (txOut ^. TxOutTxId `in_` valList txOutIds)
+                  pure (txOut ^. TxOutAddress, txOut ^. TxOutValue)
+    let outputs = map convert outrows
+    pure $ CTxBrief
+            { ctbId = CTxHash . CHash $ bsBase16Encode txhash
+            , ctbTimeIssued = Just $ utcTimeToPOSIXSeconds utctime
+            , ctbInputs = map (Just . fmap (mkCCoin . fromIntegral)) inputs
+            , ctbOutputs = map (fmap (mkCCoin . fromIntegral)) outputs
+            , ctbInputSum = mkCCoin . fromIntegral . sum $ map snd inputs
+            , ctbOutputSum = mkCCoin . fromIntegral . sum $ map snd outputs
+            }
+  where
+    convert :: (Value Text, Value Word64) -> (CAddress, Word64)
+    convert (Value addr, Value coin) = (CAddress addr, coin)
