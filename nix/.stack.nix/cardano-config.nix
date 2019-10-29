454077716b045d59960aac62bc8ccf2ded2a8723
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-30 06:50:41 +1100

    Nix voodoo

--- nix/.stack.nix/cardano-config.nix
+++ nix/.stack.nix/cardano-config.nix
@@ -1,4 +1,43 @@
-{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
   {
     flags = {};
     package = {
@@ -13,38 +52,40 @@
       synopsis = "";
       description = "";
       buildType = "Simple";
+      isLocal = true;
       };
     components = {
       "library" = {
         depends = [
-          (hsPkgs.base)
-          (hsPkgs.aeson)
-          (hsPkgs.async)
-          (hsPkgs.bytestring)
-          (hsPkgs.cardano-crypto-wrapper)
-          (hsPkgs.cardano-ledger)
-          (hsPkgs.cardano-prelude)
-          (hsPkgs.cardano-prelude-test)
-          (hsPkgs.cardano-shell)
-          (hsPkgs.cborg)
-          (hsPkgs.contra-tracer)
-          (hsPkgs.generic-monoid)
-          (hsPkgs.iohk-monitoring)
-          (hsPkgs.lobemo-backend-aggregation)
-          (hsPkgs.lobemo-backend-editor)
-          (hsPkgs.lobemo-backend-ekg)
-          (hsPkgs.lobemo-backend-monitoring)
-          (hsPkgs.lobemo-scribe-systemd)
-          (hsPkgs.network)
-          (hsPkgs.optparse-applicative)
-          (hsPkgs.ouroboros-consensus)
-          (hsPkgs.ouroboros-network)
-          (hsPkgs.iproute)
-          (hsPkgs.safe-exceptions)
-          (hsPkgs.string-conv)
-          (hsPkgs.stm)
-          (hsPkgs.text)
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."aeson" or (buildDepError "aeson"))
+          (hsPkgs."async" or (buildDepError "async"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."cardano-crypto-wrapper" or (buildDepError "cardano-crypto-wrapper"))
+          (hsPkgs."cardano-ledger" or (buildDepError "cardano-ledger"))
+          (hsPkgs."cardano-prelude" or (buildDepError "cardano-prelude"))
+          (hsPkgs."cardano-prelude-test" or (buildDepError "cardano-prelude-test"))
+          (hsPkgs."cardano-shell" or (buildDepError "cardano-shell"))
+          (hsPkgs."cborg" or (buildDepError "cborg"))
+          (hsPkgs."contra-tracer" or (buildDepError "contra-tracer"))
+          (hsPkgs."generic-monoid" or (buildDepError "generic-monoid"))
+          (hsPkgs."iohk-monitoring" or (buildDepError "iohk-monitoring"))
+          (hsPkgs."lobemo-backend-aggregation" or (buildDepError "lobemo-backend-aggregation"))
+          (hsPkgs."lobemo-backend-editor" or (buildDepError "lobemo-backend-editor"))
+          (hsPkgs."lobemo-backend-ekg" or (buildDepError "lobemo-backend-ekg"))
+          (hsPkgs."lobemo-backend-monitoring" or (buildDepError "lobemo-backend-monitoring"))
+          (hsPkgs."lobemo-scribe-systemd" or (buildDepError "lobemo-scribe-systemd"))
+          (hsPkgs."network" or (buildDepError "network"))
+          (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+          (hsPkgs."ouroboros-consensus" or (buildDepError "ouroboros-consensus"))
+          (hsPkgs."ouroboros-network" or (buildDepError "ouroboros-network"))
+          (hsPkgs."iproute" or (buildDepError "iproute"))
+          (hsPkgs."safe-exceptions" or (buildDepError "safe-exceptions"))
+          (hsPkgs."string-conv" or (buildDepError "string-conv"))
+          (hsPkgs."stm" or (buildDepError "stm"))
+          (hsPkgs."text" or (buildDepError "text"))
           ];
+        buildable = true;
         };
       };
     } // {
--- nix/.stack.nix/cardano-explorer-node.nix
+++ nix/.stack.nix/cardano-explorer-node.nix
@@ -1,4 +1,43 @@
-{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
   {
     flags = {};
     package = {
@@ -13,80 +52,87 @@
       synopsis = "The DB node for the Cardano Block Explorer";
       description = "A Cardano node that follows the Cardano chain and inserts data from the\nchain into a PostgresQL database.";
       buildType = "Simple";
+      isLocal = true;
       };
     components = {
       "library" = {
         depends = [
-          (hsPkgs.base)
-          (hsPkgs.base16-bytestring)
-          (hsPkgs.bytestring)
-          (hsPkgs.cardano-binary)
-          (hsPkgs.cardano-config)
-          (hsPkgs.cardano-crypto)
-          (hsPkgs.cardano-crypto-wrapper)
-          (hsPkgs.cardano-explorer-db)
-          (hsPkgs.cardano-ledger)
-          (hsPkgs.cardano-node)
-          (hsPkgs.cardano-prelude)
-          (hsPkgs.cardano-shell)
-          (hsPkgs.cborg)
-          (hsPkgs.containers)
-          (hsPkgs.cryptonite)
-          (hsPkgs.extra)
-          (hsPkgs.formatting)
-          (hsPkgs.io-sim-classes)
-          (hsPkgs.iohk-monitoring)
-          (hsPkgs.memory)
-          (hsPkgs.monad-logger)
-          (hsPkgs.optparse-applicative)
-          (hsPkgs.network)
-          (hsPkgs.ouroboros-consensus)
-          (hsPkgs.ouroboros-network)
-          (hsPkgs.persistent)
-          (hsPkgs.prometheus)
-          (hsPkgs.reflection)
-          (hsPkgs.serialise)
-          (hsPkgs.stm)
-          (hsPkgs.text)
-          (hsPkgs.time)
-          (hsPkgs.transformers)
-          (hsPkgs.typed-protocols)
-          (hsPkgs.typed-protocols-cbor)
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."base16-bytestring" or (buildDepError "base16-bytestring"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."cardano-binary" or (buildDepError "cardano-binary"))
+          (hsPkgs."cardano-config" or (buildDepError "cardano-config"))
+          (hsPkgs."cardano-crypto" or (buildDepError "cardano-crypto"))
+          (hsPkgs."cardano-crypto-wrapper" or (buildDepError "cardano-crypto-wrapper"))
+          (hsPkgs."cardano-explorer-db" or (buildDepError "cardano-explorer-db"))
+          (hsPkgs."cardano-ledger" or (buildDepError "cardano-ledger"))
+          (hsPkgs."cardano-node" or (buildDepError "cardano-node"))
+          (hsPkgs."cardano-prelude" or (buildDepError "cardano-prelude"))
+          (hsPkgs."cardano-shell" or (buildDepError "cardano-shell"))
+          (hsPkgs."cborg" or (buildDepError "cborg"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."cryptonite" or (buildDepError "cryptonite"))
+          (hsPkgs."extra" or (buildDepError "extra"))
+          (hsPkgs."formatting" or (buildDepError "formatting"))
+          (hsPkgs."io-sim-classes" or (buildDepError "io-sim-classes"))
+          (hsPkgs."iohk-monitoring" or (buildDepError "iohk-monitoring"))
+          (hsPkgs."memory" or (buildDepError "memory"))
+          (hsPkgs."monad-logger" or (buildDepError "monad-logger"))
+          (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+          (hsPkgs."network" or (buildDepError "network"))
+          (hsPkgs."ouroboros-consensus" or (buildDepError "ouroboros-consensus"))
+          (hsPkgs."ouroboros-network" or (buildDepError "ouroboros-network"))
+          (hsPkgs."persistent" or (buildDepError "persistent"))
+          (hsPkgs."prometheus" or (buildDepError "prometheus"))
+          (hsPkgs."reflection" or (buildDepError "reflection"))
+          (hsPkgs."serialise" or (buildDepError "serialise"))
+          (hsPkgs."stm" or (buildDepError "stm"))
+          (hsPkgs."text" or (buildDepError "text"))
+          (hsPkgs."time" or (buildDepError "time"))
+          (hsPkgs."transformers" or (buildDepError "transformers"))
+          (hsPkgs."typed-protocols" or (buildDepError "typed-protocols"))
+          (hsPkgs."typed-protocols-cbor" or (buildDepError "typed-protocols-cbor"))
           ];
+        buildable = true;
         };
       exes = {
         "cardano-explorer-node" = {
           depends = [
-            (hsPkgs.base)
-            (hsPkgs.bytestring)
-            (hsPkgs.cardano-config)
-            (hsPkgs.cardano-crypto-wrapper)
-            (hsPkgs.cardano-explorer-db)
-            (hsPkgs.cardano-explorer-node)
-            (hsPkgs.cardano-ledger)
-            (hsPkgs.cardano-node)
-            (hsPkgs.cardano-prelude)
-            (hsPkgs.cardano-binary)
-            (hsPkgs.cborg)
-            (hsPkgs.formatting)
-            (hsPkgs.cardano-shell)
-            (hsPkgs.ouroboros-consensus)
-            (hsPkgs.io-sim-classes)
-            (hsPkgs.iohk-monitoring)
-            (hsPkgs.network)
-            (hsPkgs.optparse-applicative)
-            (hsPkgs.ouroboros-network)
-            (hsPkgs.reflection)
-            (hsPkgs.serialise)
-            (hsPkgs.text)
-            (hsPkgs.typed-protocols)
-            (hsPkgs.typed-protocols-cbor)
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."cardano-config" or (buildDepError "cardano-config"))
+            (hsPkgs."cardano-crypto-wrapper" or (buildDepError "cardano-crypto-wrapper"))
+            (hsPkgs."cardano-explorer-db" or (buildDepError "cardano-explorer-db"))
+            (hsPkgs."cardano-explorer-node" or (buildDepError "cardano-explorer-node"))
+            (hsPkgs."cardano-ledger" or (buildDepError "cardano-ledger"))
+            (hsPkgs."cardano-node" or (buildDepError "cardano-node"))
+            (hsPkgs."cardano-prelude" or (buildDepError "cardano-prelude"))
+            (hsPkgs."cardano-binary" or (buildDepError "cardano-binary"))
+            (hsPkgs."cborg" or (buildDepError "cborg"))
+            (hsPkgs."formatting" or (buildDepError "formatting"))
+            (hsPkgs."cardano-shell" or (buildDepError "cardano-shell"))
+            (hsPkgs."ouroboros-consensus" or (buildDepError "ouroboros-consensus"))
+            (hsPkgs."io-sim-classes" or (buildDepError "io-sim-classes"))
+            (hsPkgs."iohk-monitoring" or (buildDepError "iohk-monitoring"))
+            (hsPkgs."network" or (buildDepError "network"))
+            (hsPkgs."optparse-applicative" or (buildDepError "optparse-applicative"))
+            (hsPkgs."ouroboros-network" or (buildDepError "ouroboros-network"))
+            (hsPkgs."reflection" or (buildDepError "reflection"))
+            (hsPkgs."serialise" or (buildDepError "serialise"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."typed-protocols" or (buildDepError "typed-protocols"))
+            (hsPkgs."typed-protocols-cbor" or (buildDepError "typed-protocols-cbor"))
             ];
+          buildable = true;
           };
         };
       tests = {
         "test" = {
-          depends = [ (hsPkgs.base) (hsPkgs.cardano-explorer-node) ];
+          depends = [
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."cardano-explorer-node" or (buildDepError "cardano-explorer-node"))
+            ];
+          buildable = true;
           };
         };
       };
