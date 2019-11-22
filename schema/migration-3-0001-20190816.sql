15cdf55c2ef68898ea22859021deb844894d0cd3
Sam Jeston <sam.jeston@iohk.io>
2019-11-22 15:18:47 +1000

    Add cardano-graphql views

--- schema/migration-3-0001-20190816.sql
+++ schema/migration-3-0001-20190816.sql
@@ -14,19 +14,134 @@ create view utxo_view as select
 	on tx_out.tx_id = tx_in.tx_out_id and tx_out.index = tx_in.tx_out_index
   where tx_in.tx_in_id is null ;
 
+-- START: cardano-graphql views
+create view "Utxo" as select
+  address,
+  value,
+  tx.hash as "txId",
+  index
+from tx
+join tx_out
+  on tx.id = tx_out.tx_id
+left outer join tx_in
+  on tx_out.tx_id = tx_in.tx_out_id
+  and tx_out.index = tx_in.tx_out_index
+where tx_in.tx_in_id is null;
 
--- Views for the explorer Javascript GraphQL interface.
-create view TransactionOutput as select
-	address,
-	tx.hash as txId,
-	index
-  from tx inner join tx_out
-	on tx.id = tx_out.tx_id ;
+create view "TransactionOutput" as
+select
+  address,
+  value,
+  tx.hash as "txId",
+  index
+from tx
+join tx_out
+  on tx.id = tx_out.tx_id;
 
-create view TransactionInput as select
-	address,
-	tx.hash as sourceTxId,
-	tx_in.tx_out_index as sourceTxIndex,
-	value
-  from tx inner join tx_out on tx.id = tx_out.tx_id
-	inner join tx_in on tx_in.tx_out_id = tx.id ;
+create view "TransactionInput" as
+select
+  source_tx_out.address,
+  source_tx_out.value,
+  tx.hash as "txId",
+  source_tx.hash as "sourceTxId",
+  tx_in.tx_out_index as "sourceTxIndex"
+from
+  tx
+join tx_in
+  on tx_in.tx_in_id = tx.id
+join tx_out as source_tx_out
+  on tx_in.tx_out_id = source_tx_out.tx_id
+  and tx_in.tx_out_index = source_tx_out.index
+join tx as source_tx
+  on source_tx_out.tx_id = source_tx.id;
+
+create view "Block" as
+select
+  CAST(COALESCE((select sum(tx.fee) from tx where tx.block = block.id), 0) as integer) as "fees",
+  block."hash" as id,
+  block.merkel_root as "merkelRootHash",
+  block.block_no as number,
+  previous_block."hash" as "previousBlockId",
+  next_block."hash" as "nextBlockId",
+  slot_leader."description" as "createdBy",
+  block.size as size,
+	block.tx_count as "transactionCount",
+  -- Even though we have epochNo defined in the Slot view,
+  -- this is written by the node-client and makes identification
+  -- of EBBs simpler, as EBBs don't have a slot_no
+  block.epoch_no as "epochNo",
+  block.slot_no as "slotNo",
+  block.slot_no - (block.epoch_no * (10 * (select protocol_const from meta))) as "slotWithinEpoch",
+  block.time as "createdAt"
+from block
+left outer join block as previous_block
+  on block.previous = previous_block.id
+left outer join block as next_block
+  on next_block.previous = block.id
+left outer join slot_leader
+  on block.slot_leader = slot_leader.id;
+
+create view "Transaction" as
+select
+  block.hash as "blockId",
+  COALESCE(tx.fee, 0) as fee,
+  tx.hash as id,
+  cast((select sum("value") from tx_out where tx_id = tx.id) as bigint) as "totalOutput",
+	tx.size,
+  block.time as "includedAt"
+from
+  tx
+inner join block
+  on block.id = tx.block;
+
+create view "Epoch" as
+select
+  cast(sum(tx_out.value) as bigint) as output,
+  count(distinct tx.hash) as "transactionsCount",
+  block.epoch_no as "number",
+  min(block.time) as "startedAt",
+  max(block.time) as "lastBlockTime"
+from block
+join tx
+  on tx.block = block.id
+join tx_out
+  on tx_out.tx_id = tx.id
+where epoch_no is not null
+group by block.epoch_no
+order by block.epoch_no;
+
+-- This function plays really nicely with Hasura,
+-- and allows us to query the utxo set at any block height
+-- https://docs.hasura.io/1.0/graphql/manual/queries/custom-functions.html
+CREATE FUNCTION utxo_set_at_block("blockId" hash32type)
+RETURNS SETOF "TransactionOutput" AS $$
+  select
+    "TransactionOutput".address,
+    "TransactionOutput".value,
+    "TransactionOutput"."txId",
+    "TransactionOutput".index
+  from tx
+  join tx_out
+    on tx.id = tx_out.tx_id
+  join "TransactionOutput"
+    on tx.hash = "TransactionOutput"."txId"
+  left outer join tx_in
+    on tx_out.tx_id = tx_in.tx_out_id
+    and tx_out.index = tx_in.tx_out_index
+  where tx_in.tx_in_id is null
+  and tx.block <= (select id from block where hash = "blockId")
+$$ LANGUAGE sql STABLE;
+
+create view "Cardano" as
+select
+  number as "blockHeight",
+  "epochNo" as "currentEpochNo",
+  (select slot_duration from meta) as "slotDuration",
+  (select start_time from meta) as "startTime",
+  (select protocol_const from meta) as "protocolConst",
+	(select network_name from meta) as "networkName"
+from "Block"
+where number is not null
+order by number desc
+limit 1;
+-- END: cardano-graphql views
