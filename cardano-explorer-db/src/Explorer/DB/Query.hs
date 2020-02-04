dac4c39fec84ca0b8a536c5c4a8da555625735f9
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-02-04 15:52:28 +1100

    db: Remove redundant query

--- cardano-explorer-db/src/Explorer/DB/Query.hs
+++ cardano-explorer-db/src/Explorer/DB/Query.hs
@@ -20,7 +20,6 @@ module Explorer.DB.Query
   , queryLatestBlock
   , queryLatestBlockId
   , queryLatestBlockNo
-  , queryLatestBlocks
   , queryLatestSlotNo
   , queryMeta
   , queryNetworkName
@@ -246,24 +245,6 @@ queryLatestBlock = do
                 limit 1
                 pure $ blk
   pure $ fmap entityVal (listToMaybe res)
-
--- | Get the last N blocks.
--- This assumes that the block are inserted into the datebase from oldest to
--- newest which is not exlicitly enforced, but should arise automatically
--- from the way blocks are retrieved and inserted into the database.
-queryLatestBlocks :: MonadIO m => Int -> ReaderT SqlBackend m [(Word64, ByteString)]
-queryLatestBlocks limitCount = do
-    eblks <- select $ from $ \ blk -> do
-                orderBy [desc (blk ^. BlockId)]
-                limit $ fromIntegral limitCount
-                pure $ (blk ^. BlockSlotNo, blk ^. BlockHash)
-    pure $ catMaybes (map convert eblks)
-  where
-    convert :: (Value (Maybe Word64), Value ByteString) -> Maybe (Word64, ByteString)
-    convert (va, vb) =
-      case (unValue va, unValue vb) of
-        (Nothing, _ ) -> Nothing
-        (Just a, b) -> Just (a, b)
 
 -- | Given a 'BlockId' return the 'BlockId' of the previous block.
 queryPreviousBlockNo :: MonadIO m => Word64 -> ReaderT SqlBackend m (Maybe Word64)
