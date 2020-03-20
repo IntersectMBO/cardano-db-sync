63b1ea0d08a9c34bc37eaf90f9872dda889c127d
Alexander Diemand <codieplusplus@apax.net>
2020-03-20 07:06:03 +0100

    CAD-345 remaining ToObject instance
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- cardano-db-sync/src/Cardano/DbSync/Tracing/ToObjectOrphans.hs
+++ cardano-db-sync/src/Cardano/DbSync/Tracing/ToObjectOrphans.hs
@@ -8,52 +8,17 @@ module Cardano.DbSync.Tracing.ToObjectOrphans () where
 
 import           Data.Text (Text)
 import           Data.Aeson ((.=))
-import           Data.Functor.Identity (Identity (..))
 
 import           Cardano.BM.Data.Tracer
-import           Cardano.BM.Tracing
 
 import           Cardano.Tracing.ToObjectOrphans (defaultTextTransformer)
 
-import           Ouroboros.Consensus.Byron.Ledger (ByronBlock (..))
+import           Ouroboros.Consensus.Byron.Ledger (ByronBlock)
 import           Ouroboros.Network.Block (Tip)
 import           Ouroboros.Network.Codec (AnyMessage)
-import           Ouroboros.Network.NodeToClient (LocalAddress, TraceSendRecv)
+import           Ouroboros.Network.NodeToClient (TraceSendRecv)
 import           Ouroboros.Network.Protocol.ChainSync.Type (ChainSync)
-import           Ouroboros.Network.Subscription (SubscriptionTrace (..))
 
-
-instance HasPrivacyAnnotation (Identity (SubscriptionTrace LocalAddress))
-
-instance HasSeverityAnnotation (Identity (SubscriptionTrace LocalAddress)) where
-  getSeverityAnnotation (Identity ev) = case ev of
-    SubscriptionTraceConnectStart {} -> Info
-    SubscriptionTraceConnectEnd {} -> Info
-    SubscriptionTraceConnectException {} -> Error
-    SubscriptionTraceSocketAllocationException {} -> Error
-    SubscriptionTraceTryConnectToPeer {} -> Info
-    SubscriptionTraceSkippingPeer {} -> Info
-    SubscriptionTraceSubscriptionRunning -> Debug
-    SubscriptionTraceSubscriptionWaiting {} -> Debug
-    SubscriptionTraceSubscriptionFailed -> Warning
-    SubscriptionTraceSubscriptionWaitingNewConnection {} -> Debug
-    SubscriptionTraceStart {} -> Debug
-    SubscriptionTraceRestart {} -> Debug
-    SubscriptionTraceConnectionExist {} -> Info
-    SubscriptionTraceUnsupportedRemoteAddr {} -> Warning
-    SubscriptionTraceMissingLocalAddress -> Warning
-    SubscriptionTraceApplicationException {} -> Error
-    SubscriptionTraceAllocateSocket {} -> Debug
-    SubscriptionTraceCloseSocket {} -> Debug
-
-instance Transformable Text IO (Identity (SubscriptionTrace LocalAddress)) where
-  trTransformer = defaultTextTransformer
-
-instance ToObject (Identity (SubscriptionTrace LocalAddress)) where
-  toObject _verb (Identity ev) =
-    mkObject [ "kind" .= ("SubscriptionTrace" :: String)
-             , "event" .= show ev
-             ]
 
 instance Transformable Text IO (TraceSendRecv (ChainSync ByronBlock (Tip ByronBlock))) where
   trTransformer = defaultTextTransformer
