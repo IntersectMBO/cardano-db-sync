ab684bdcf40963447d554c5ecc16cdc501220f53
Matthew Eric Bassett <matthew@emurgo.io>
2020-06-25 13:32:30 +0100

    mod - source formatting, attempting to much an unspecified standard.

--- cardano-db-sync/src/Cardano/DbSync/Plugin/TxBody.hs
+++ cardano-db-sync/src/Cardano/DbSync/Plugin/TxBody.hs
@@ -6,63 +6,63 @@ module Cardano.DbSync.Plugin.TxBody
   , insertTxBody
   )
 where
-import           Cardano.Binary                 ( serialize' )
-import           Cardano.BM.Trace               ( Trace
-                                                )
+import           Cardano.Binary ( serialize' )
+import           Cardano.BM.Trace ( Trace )
 
-import           Control.Monad.Logger           ( LoggingT )
+import           Control.Monad.Logger ( LoggingT )
 import           Control.Monad.Trans.Except.Extra
-import           Control.Monad.Trans.Reader     ( ReaderT )
+import           Control.Monad.Trans.Reader ( ReaderT )
 
-import qualified Cardano.Chain.Block           as Ledger
-import qualified Cardano.Chain.UTxO            as Ledger
+import qualified Cardano.Chain.Block as Ledger
+import qualified Cardano.Chain.UTxO as Ledger
 
-import           Cardano.Crypto                 ( serializeCborHash )
+import           Cardano.Crypto ( serializeCborHash )
 
-import qualified Cardano.Db                    as DB
+import qualified Cardano.Db as DB
 import           Cardano.DbSync.Error
 import           Cardano.DbSync.Plugin
 import           Cardano.DbSync.Util
 
 import           Cardano.Prelude
 
+import           Database.Persist.Sql ( SqlBackend )
 
-import           Database.Persist.Sql           ( SqlBackend )
-
-import           Ouroboros.Consensus.Byron.Ledger
-                                                ( ByronBlock(..) )
-import           Ouroboros.Network.Block        ( Tip )
+import           Ouroboros.Consensus.Byron.Ledger ( ByronBlock(..) )
+import           Ouroboros.Network.Block ( Tip )
 
 
 insertTxBody
-  :: Trace IO Text
-  -> ByronBlock
-  -> Tip ByronBlock
-  -> ReaderT SqlBackend (LoggingT IO) (Either DbSyncNodeError ())
-insertTxBody _ blk _ = runExceptT $ case byronBlockRaw blk of
-  Ledger.ABOBBlock ablk -> insertBlock ablk
-  _                     -> return ()
+    :: Trace IO Text -> ByronBlock -> Tip ByronBlock
+    -> ReaderT SqlBackend (LoggingT IO) (Either DbSyncNodeError ())
+insertTxBody _ blk _ = 
+  runExceptT $ 
+    case byronBlockRaw blk of
+      Ledger.ABOBBlock ablk -> insertBlock ablk
+      _ -> return ()
  where
   insertBlock
-    :: MonadIO m
-    => Ledger.ABlock ByteString
-    -> ExceptT DbSyncNodeError (ReaderT SqlBackend m) ()
+      :: MonadIO m
+      => Ledger.ABlock ByteString
+      -> ExceptT DbSyncNodeError (ReaderT SqlBackend m) ()
   insertBlock ablk =
     mapM_ insertTx $ blockPayload ablk
 
   insertTx
-    :: MonadIO m
-    => Ledger.TxAux
-    -> ExceptT DbSyncNodeError (ReaderT SqlBackend m) ()
+      :: MonadIO m
+      => Ledger.TxAux
+      -> ExceptT DbSyncNodeError (ReaderT SqlBackend m) ()
   insertTx tx =
     let hash = unTxHash $ serializeCborHash (Ledger.taTx tx)
         body = serialize' $ Ledger.taTx tx
-    in  void $ lift . DB.insertTxBody $ DB.TxBody { DB.txBodyHash = hash
-                                                  , DB.txBodyBody = body
-                                                  }
+    in  
+      void $ lift . DB.insertTxBody $ DB.TxBody { DB.txBodyHash = hash
+                                                , DB.txBodyBody = body
+                                                }
 
 defDbSyncTxBodyPlugin :: DbSyncNodePlugin
-defDbSyncTxBodyPlugin = DbSyncNodePlugin { plugOnStartup     = []
-                                         , plugInsertBlock   = [insertTxBody]
-                                         , plugRollbackBlock = []
-                                         }
+defDbSyncTxBodyPlugin = 
+  DbSyncNodePlugin 
+    { plugOnStartup     = []
+    , plugInsertBlock   = [insertTxBody]
+    , plugRollbackBlock = []
+    }
