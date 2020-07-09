e87266afd00a0de4d304befe5b04d41786bde68a
Matthew Eric Bassett <matthew@emurgo.io>
2020-07-09 16:16:59 +0100

    fix - rebasing

--- cardano-db-sync/src/Cardano/DbSync/Plugin/TxBody.hs
+++ cardano-db-sync/src/Cardano/DbSync/Plugin/TxBody.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -10,6 +11,7 @@ import           Cardano.Binary ( serialize' )
 import           Cardano.BM.Trace ( Trace )
 
 import           Control.Monad.Logger ( LoggingT )
+import           Control.Monad.Trans.Control (MonadBaseControl)
 import           Control.Monad.Trans.Except.Extra
 import           Control.Monad.Trans.Reader ( ReaderT )
 
@@ -19,40 +21,48 @@ import qualified Cardano.Chain.UTxO as Ledger
 import           Cardano.Crypto ( serializeCborHash )
 
 import qualified Cardano.Db as DB
+import qualified Cardano.DbSync.Era.Byron.Util as Byron
 import           Cardano.DbSync.Error
 import           Cardano.DbSync.Plugin
-import           Cardano.DbSync.Util
+import           Cardano.DbSync.Types
 
 import           Cardano.Prelude
 
 import           Database.Persist.Sql ( SqlBackend )
 
 import           Ouroboros.Consensus.Byron.Ledger ( ByronBlock(..) )
-import           Ouroboros.Network.Block ( Tip )
 
 
-insertTxBody
-    :: Trace IO Text -> ByronBlock -> Tip ByronBlock
+insertTxBody 
+    :: Trace IO Text -> DbSyncEnv -> CardanoBlockTip
     -> ReaderT SqlBackend (LoggingT IO) (Either DbSyncNodeError ())
-insertTxBody _ blk _ = 
+insertTxBody _ _ blkTip  =
+  case blkTip of
+    ByronBlockTip blk _ -> insertByronBody blk
+    _ -> return $ Right ()
+
+insertByronBody
+    :: ByronBlock
+    -> ReaderT SqlBackend (LoggingT IO) (Either DbSyncNodeError ())
+insertByronBody blk = 
   runExceptT $ 
     case byronBlockRaw blk of
       Ledger.ABOBBlock ablk -> insertBlock ablk
       _ -> return ()
  where
   insertBlock
-      :: MonadIO m
+      :: (MonadBaseControl IO m, MonadIO m)
       => Ledger.ABlock ByteString
       -> ExceptT DbSyncNodeError (ReaderT SqlBackend m) ()
   insertBlock ablk =
-    mapM_ insertTx $ blockPayload ablk
+    mapM_ insertTx $ Byron.blockPayload ablk
 
   insertTx
-      :: MonadIO m
+      :: (MonadBaseControl IO m, MonadIO m)
       => Ledger.TxAux
       -> ExceptT DbSyncNodeError (ReaderT SqlBackend m) ()
   insertTx tx =
-    let hash = unTxHash $ serializeCborHash (Ledger.taTx tx)
+    let hash = Byron.unTxHash $ serializeCborHash (Ledger.taTx tx)
         body = serialize' $ Ledger.taTx tx
     in  
       void $ lift . DB.insertTxBody $ DB.TxBody { DB.txBodyHash = hash
