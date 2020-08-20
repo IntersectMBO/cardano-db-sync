482a6d4ac81868e8281d4960a4816e5edd7c562b
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-08-20 16:58:04 +1000

    db-tool: Add validation that all pools have owners

--- cardano-db/app/Cardano/Db/App/Validation.hs
+++ cardano-db/app/Cardano/Db/App/Validation.hs
@@ -2,12 +2,14 @@ module Cardano.Db.App.Validation
   ( runValidation
   ) where
 
+import           Cardano.Db.App.Validate.BlockTxs (validateEpochBlockTxs)
 import           Cardano.Db.App.Validate.EpochTable (validateEpochTable)
 import           Cardano.Db.App.Validate.TotalSupply (validateTotalSupplyDecreasing)
-import           Cardano.Db.App.Validate.BlockTxs (validateEpochBlockTxs)
+import           Cardano.Db.App.Validate.PoolOwner (validateAllPoolsHaveOwners)
 
 runValidation :: IO ()
 runValidation = do
   validateTotalSupplyDecreasing
   validateEpochTable
   validateEpochBlockTxs
+  validateAllPoolsHaveOwners
