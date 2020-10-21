465d581e8c53a3d2affbb2c5fa96a7612f513634
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-10-21 17:29:19 +1100

    db: Add an extra migration test
    Run the migrations, grab the schema version from the database and
    compare it to the schema version according to the migration files.
    
    Closes: https://github.com/input-output-hk/cardano-db-sync/issues/369

--- cardano-db/test/Test/IO/Cardano/Db/Migration.hs
+++ cardano-db/test/Test/IO/Cardano/Db/Migration.hs
@@ -5,6 +5,11 @@ module Test.IO.Cardano.Db.Migration
 
 import           Cardano.Db
 
+import           Control.Monad (unless)
+
+import qualified Data.List as List
+import           Data.Maybe (fromMaybe)
+
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.HUnit (testCase)
 
@@ -18,5 +23,42 @@ tests =
 -- Really just make sure that the migrations do actually run correctly.
 -- If they fail the file path of the log file (in /tmp) will be printed.
 migrationTest :: IO ()
-migrationTest =
-  runMigrations id True (MigrationDir "../schema") (Just $ LogFileDir "/tmp")
+migrationTest = do
+  let schemaDir = MigrationDir "../schema"
+  runMigrations id True schemaDir (Just $ LogFileDir "/tmp")
+  expected <- readSchemaVersion schemaDir
+  actual <- getDbSchemaVersion
+  unless (expected == actual) $
+    error $ mconcat
+            [ "Schema version mismatch. Expected "
+            , showSchemaVersion expected
+            , " but got "
+            , showSchemaVersion actual
+            , "."
+            ]
+
+getDbSchemaVersion :: IO SchemaVersion
+getDbSchemaVersion =
+  runDbNoLogging $
+    fromMaybe (error "getDbSchemaVersion: Nothing") <$> querySchemaVersion
+
+readSchemaVersion :: MigrationDir -> IO SchemaVersion
+readSchemaVersion migrationDir = do
+    xs <- filter ignoreVersion . fmap fst <$> getMigrationScripts migrationDir
+    pure $ List.foldl' func (SchemaVersion 0 0 0) xs
+  where
+    func :: SchemaVersion -> MigrationVersion -> SchemaVersion
+    func (SchemaVersion a b c) mv =
+      case mvStage mv of
+        1 -> SchemaVersion (max (mvVersion mv) a) b c
+        2 -> SchemaVersion a (max (mvVersion mv) b) c
+        3 -> SchemaVersion a b (max (mvVersion mv) c)
+        _otherwise -> SchemaVersion a b c
+
+    -- Ignore all migrations with version numbers of 9000 or above.
+    ignoreVersion :: MigrationVersion -> Bool
+    ignoreVersion mv = mvVersion mv < 9000
+
+showSchemaVersion :: SchemaVersion -> String
+showSchemaVersion (SchemaVersion a b c) =
+  List.intercalate "." [show a, show b, show c]
