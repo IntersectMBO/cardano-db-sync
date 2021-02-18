5afd8d1d72827c8eb924c1c5bf67fb7c328f6bd0
James Chong <wei3hua2@gmail.com>
2021-02-18 22:14:43 +0800

    fixed: Fail to run migration if postgresql port number is not default (5432)

--- cardano-db/src/Cardano/Db/Migration.hs
+++ cardano-db/src/Cardano/Db/Migration.hs
@@ -86,6 +86,7 @@ applyMigration quiet pgconfig mLogFilename logHandle (version, script) = do
             , "--quiet"
             , "--username=" <> BS.unpack (pgcUser pgconfig)
             , "--host=" <> BS.unpack (pgcHost pgconfig)
+            , "--port=" <> BS.unpack (pgcPort pgconfig)
             , "--no-psqlrc"                     -- Ignore the ~/.psqlrc file.
             , "--single-transaction"            -- Run the file as a transaction.
             , "--set ON_ERROR_STOP=on"          -- Exit with non-zero on error.
