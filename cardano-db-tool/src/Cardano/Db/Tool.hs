a928b84dc4962b3fa1eeee579ddcc8d46ecf21f8
kderme <kostas.dermentzis@iohk.io>
2021-05-24 20:14:19 +0300

    Add a db-tool utility which prepares a snapshot
    It will rollback the db if it's not synced with the ledger state.
    
    Co-Authored-By: Erik de Castro Lopo <erikd@mega-nerd.com>
    Closes: https://github.com/input-output-hk/cardano-db-sync/issues/613

--- cardano-db-tool/src/Cardano/Db/Tool.hs
+++ cardano-db-tool/src/Cardano/Db/Tool.hs
@@ -2,5 +2,6 @@ module Cardano.Db.Tool
   ( module X
   ) where
 
+import           Cardano.Db.Tool.PrepareSnapshot as X
 import           Cardano.Db.Tool.UtxoSet as X
 import           Cardano.Db.Tool.Validation as X
---
+++ cardano-db-tool/src/Cardano/Db/Tool/PrepareSnapshot.hs
@@ -0,0 +1,104 @@
+module Cardano.Db.Tool.PrepareSnapshot
+  ( PrepareSnapshotArgs (..)
+  , prepareSnapshot
+  ) where
+
+import           Cardano.Prelude (Word64, fromMaybe)
+
+import           Control.Monad
+
+import           Cardano.Db
+import           Cardano.Sync.Config.Types hiding (LogFileDir, MigrationDir)
+import           Cardano.Sync.LedgerState
+
+import qualified Data.ByteString.Base16 as Base16
+import           Data.Version (versionBranch)
+
+import           Ouroboros.Network.Block hiding (blockHash)
+
+import           Paths_cardano_db_tool (version)
+
+import           System.IO (hFlush, stdout)
+
+newtype PrepareSnapshotArgs = PrepareSnapshotArgs
+  { unPrepareSnapshotArgs :: LedgerStateDir
+  }
+
+prepareSnapshot :: PrepareSnapshotArgs -> IO ()
+prepareSnapshot = prepareSnapshotAux True
+
+prepareSnapshotAux :: Bool -> PrepareSnapshotArgs -> IO ()
+prepareSnapshotAux firstTry args = do
+    ledgerFiles <- listLedgerStateFilesOrdered (unPrepareSnapshotArgs args)
+    mblock <- runDbNoLogging queryLatestBlock
+    case mblock of
+      Just block | Just bSlotNo <- SlotNo <$> blockSlotNo block -> do
+        let bHash = blockHash block
+        let (newerFiles, mfile) = findLedgerStateFile ledgerFiles (bSlotNo, bHash)
+        printNewerSnapshots newerFiles
+        case mfile of
+            Right file -> do
+              let bblockNo = fromMaybe 0 $ blockBlockNo block
+              printCreateSnapshot bblockNo (lsfFilePath file)
+            Left (file : _) -> do
+                -- We couldn't find the tip of the db, so we return a list of
+                -- the available ledger files, before this tip.
+              putStrLn $ concat
+                [ "Ledger and db don't match. DB tip is at "
+                , show bSlotNo, " ", show (hashToAnnotation bHash)
+                , " (full ", show (Base16.encode bHash), ")"
+                , " and the closest ledger state file is at "
+                , show (lsfSlotNo file), " ", show (lsfHash file)
+                ]
+              if firstTry then do
+                interactiveRollback $ lsfSlotNo file
+                prepareSnapshotAux False args
+              else
+                putStrLn "After a rollback the db is in sync with no ledger state file"
+            Left [] ->
+              putStrLn "No ledger state file matches the db tip. You need to run db-sync before creating a snapshot"
+
+      _ -> do
+            putStrLn "The db is empty. You need to sync from genesis and then create a snapshot."
+  where
+    interactiveRollback :: SlotNo -> IO ()
+    interactiveRollback slot = do
+      putStr $ "Do you want to rollback the db to " ++ show slot  ++ " (Y/n): "
+      hFlush stdout
+      input <- getLine
+      case input of
+        "n" -> pure ()
+        _ -> do
+          putStrLn $ "Rolling back to " ++ show slot
+          runRollback slot
+          putStrLn "Rolling back done. Revalidating from scratch"
+          putStrLn ""
+
+    runRollback :: SlotNo -> IO ()
+    runRollback slot = runDbNoLogging $ do
+      slots <- querySlotNosGreaterThan $ unSlotNo slot
+      mapM_ deleteCascadeSlotNo slots
+
+    printNewerSnapshots :: [LedgerStateFile] -> IO ()
+    printNewerSnapshots newerFiles = do
+      unless (null newerFiles) $
+        putStrLn $ concat
+          [ "There are newer ledger state files, which are ignored: "
+          , show newerFiles, "\n"
+          ]
+
+    printCreateSnapshot :: Word64 -> FilePath -> IO ()
+    printCreateSnapshot bblockNo fp = do
+      let mMajor = listToMaybe $ versionBranch version
+          majorStr = case mMajor of
+                        Nothing -> ""
+                        Just majorV -> "schema-" ++ show majorV
+      putStrLn $ concat
+        [ "Create a snapshot with:\n"
+        , "    scripts/postgresql-setup.sh --create-snapshot db-sync-snapshot-"
+        , majorStr
+        , "-block-"
+        , show bblockNo
+        , "-x86_64 "
+        , fp
+        ]
