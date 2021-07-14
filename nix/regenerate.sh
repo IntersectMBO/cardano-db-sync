b925fc2a0a69e48aac39249e0aa6d9e929586f79
Erik de Castro Lopo <erikd@mega-nerd.com>
2021-07-14 17:48:44 +1000

    nix/regenerate.sh: Update to handle Nix flakes

--- nix/regenerate.sh
+++ nix/regenerate.sh
@@ -2,4 +2,14 @@
 set -euo pipefail
 cd $(git rev-parse --show-toplevel)
 
-exec $(nix-build `dirname $0`/. -A iohkNix.cabalProjectRegenerate --no-out-link --option substituters "https://hydra.iohk.io https://cache.nixos.org" --option trusted-substituters "" --option trusted-public-keys "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=")/bin/cabal-project-regenerate
+nix_version=$(nix --version | sed 's/.* //;s/p.*//')
+
+if test nix_version -lt 2.4 ; then
+  echo "Nix version 2.4 (which may be unstable) or greater required."
+  echo "To update the Nix channels: nix-channel --update"
+  echo "To switch to unstable Nix: nix-env -iA nixpkgs.nixUnstable"
+  echo "To switch to stable Nix: nix-env -iA nixpkgs.nix"
+  exit 0
+  fi
+
+nix --experimental-features "nix-command flakes" flake lock  --update-input haskellNix
