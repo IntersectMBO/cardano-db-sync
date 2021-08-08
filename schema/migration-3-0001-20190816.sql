30149d588584ac73ab80d4932fc14efac3c2b843
Erik de Castro Lopo <erikd@mega-nerd.com>
2021-08-09 07:43:36 +1000

    schema: Fix utxo_view
    This view has been broken since it was added but this issue was only
    just noticed and reported recently by @wutzebaer.

--- schema/migration-3-0001-20190816.sql
+++ schema/migration-3-0001-20190816.sql
@@ -17,10 +17,12 @@ create view utxo_byron_view as select
 
 -- A utxo view which shows all unspent transaction outputs *excluding* the un-redeemed redeem
 -- addresses.
+-- This should produce the same query results as the above `utxo_byron_view` for Shelley addresses
+-- and non-redeem Byron addresses.
 create view utxo_view as select
 	tx_out.*
   from tx_out
 	left outer join tx_in on tx_out.tx_id = tx_in.tx_out_id and tx_out.index = tx_in.tx_out_index
 	left outer join tx on tx.id = tx_out.tx_id
 	left outer join block on tx.block_id = block.id
-  where tx_in.tx_in_id is null and block.epoch_no is null ;
+  where tx_in.tx_in_id is null and block.epoch_no is not null ;
