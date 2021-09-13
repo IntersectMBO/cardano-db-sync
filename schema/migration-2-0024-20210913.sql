d03a88a8e8f9c4a2bbd6210bcc46d833f34aea69
Erik de Castro Lopo <erikd@mega-nerd.com>
2021-09-13 16:48:07 +1000

    db: Unify SQL types of epoch_stake.epoch_no and epoch.no
    These need to be the same SQL types for the Hasura code generation.
    The fact that they were not the same type was a mistake.
    
    Closes: https://github.com/input-output-hk/cardano-db-sync/issues/811

---
+++ schema/migration-2-0024-20210913.sql
@@ -0,0 +1,20 @@
+-- Persistent generated migration.
+
+CREATE FUNCTION migrate() RETURNS void AS $$
+DECLARE
+  next_version int ;
+BEGIN
+  SELECT stage_two + 1 INTO next_version FROM schema_version ;
+  IF next_version = 24 THEN
+    EXECUTE 'ALTER TABLE "epoch_stake" ALTER COLUMN "epoch_no" TYPE uinteger' ;
+    EXECUTE 'ALTER TABLE "redeemer" ADD CONSTRAINT "redeemer_datum_id_fkey" FOREIGN KEY("datum_id") REFERENCES "datum"("id") ON DELETE CASCADE  ON UPDATE RESTRICT' ;
+    -- Hand written SQL statements can be added here.
+    UPDATE schema_version SET stage_two = next_version ;
+    RAISE NOTICE 'DB has been migrated to stage_two version %', next_version ;
+  END IF ;
+END ;
+$$ LANGUAGE plpgsql ;
+
+SELECT migrate() ;
+
+DROP FUNCTION migrate() ;
