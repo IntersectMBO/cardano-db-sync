IntersectMBO/cardano-db-sync

commit e28ee284fa9084d45d2c9554f684759266cf1ffd

Purge all uses of coerce
Also use hlint to ban the reintroduction of `coerce`.

Closes: https://github.com/input-output-hk/cardano-db-sync/issues/838

cardano-db-sync/src/Cardano/DbSync/Era/Byron/Insert.hs
@@ -46,6 +46,7 @@ import           Database.Persist.Sql (SqlBackend)
 
 import qualified Cardano.Db as DB
 import qualified Cardano.DbSync.Era.Byron.Util as Byron
+import qualified Cardano.Sync.Era.Byron.Util as Byron
 import           Cardano.Sync.Error
 import           Cardano.Sync.Util
 

cardano-sync/src/Cardano/Sync/Era/Byron/Util.hs
@@ -42,7 +42,6 @@ import qualified Cardano.Chain.Update as Byron
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
-import           Data.Coerce (coerce)
 import qualified Data.Text.Encoding as Text
 
 
@@ -72,7 +71,9 @@ epochNumber blk slotsPerEpoch =
   slotNumber blk `div` slotsPerEpoch
 
 genesisToHeaderHash :: Byron.GenesisHash -> Byron.HeaderHash
-genesisToHeaderHash = coerce
+genesisToHeaderHash =
+  fromMaybe (panic "Cardano.Sync.Era.Byron.Util.genesisToHeaderHash")
+    . Crypto.abstractHashFromBytes . Crypto.abstractHashToBytes . Crypto.serializeCborHash
 
 protocolVersion :: Byron.ABlock ByteString -> Byron.ProtocolVersion
 protocolVersion = Byron.headerProtocolVersion . Byron.blockHeader

cardano-db-sync/src/Cardano/DbSync/Era/Byron/Util.hs
@@ -4,24 +4,13 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module Cardano.DbSync.Era.Byron.Util
-  ( blockHash
-  , blockNumber
-  , blockPayload
-  , blockPreviousHash
-  , boundaryEpochNumber
+  ( boundaryEpochNumber
   , configSlotDuration
-  , epochNumber
-  , genesisToHeaderHash
   , mkSlotLeader
-  , protocolVersion
-  , renderAbstractHash
   , slotLeaderHash
-  , slotNumber
   , unAbstractHash
   , unAddressHash
   , unCryptoHash
-  , unHeaderHash
-  , unTxHash
   ) where
 
 import           Cardano.Prelude hiding (catch)
@@ -36,46 +25,23 @@ import qualified Cardano.Crypto.Wallet as Crypto
 import qualified Cardano.Chain.Block as Byron
 import qualified Cardano.Chain.Common as Byron
 import qualified Cardano.Chain.Genesis as Byron
-import qualified Cardano.Chain.Slotting as Byron
-import qualified Cardano.Chain.UTxO as Byron
 import qualified Cardano.Chain.Update as Byron
+
+import qualified Cardano.Db as DB
 
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
-import           Data.Coerce (coerce)
 import qualified Data.Text.Encoding as Text
 
-import qualified Cardano.Db as DB
 
-
-blockHash :: Byron.ABlock ByteString -> ByteString
-blockHash = unHeaderHash . Byron.blockHashAnnotated
 
 boundaryEpochNumber :: Byron.ABoundaryBlock ByteString -> Word64
 boundaryEpochNumber = Byron.boundaryEpoch . Byron.boundaryHeader
 
-blockNumber :: Byron.ABlock ByteString -> Word64
-blockNumber =
-  Byron.unChainDifficulty . Byron.headerDifficulty . Byron.blockHeader
-
-blockPayload :: Byron.ABlock a -> [Byron.TxAux]
-blockPayload =
-  Byron.unTxPayload . Byron.bodyTxPayload . Byron.blockBody
-
-blockPreviousHash :: Byron.ABlock a -> Byron.HeaderHash
-blockPreviousHash = Byron.headerPrevHash . Byron.blockHeader
-
 configSlotDuration :: Byron.Config -> Word64
 configSlotDuration =
   fromIntegral . Byron.ppSlotDuration . Byron.gdProtocolParameters . Byron.configGenesisData
-
-epochNumber :: Byron.ABlock ByteString -> Word64 -> Word64
-epochNumber blk slotsPerEpoch =
-  slotNumber blk `div` slotsPerEpoch
-
-genesisToHeaderHash :: Byron.GenesisHash -> Byron.HeaderHash
-genesisToHeaderHash = coerce
 
 mkSlotLeader :: Byron.ABlock ByteString -> DB.SlotLeader
 mkSlotLeader blk =
@@ -84,34 +50,17 @@ mkSlotLeader blk =
   -- On Byrom poolHashId will always be Nothing.
   in DB.SlotLeader slHash Nothing slName
 
-protocolVersion :: Byron.ABlock ByteString -> Byron.ProtocolVersion
-protocolVersion = Byron.headerProtocolVersion . Byron.blockHeader
-
-renderAbstractHash :: Crypto.AbstractHash algo a -> Text
-renderAbstractHash =
-    Text.decodeUtf8 . Base16.encode . Crypto.abstractHashToBytes
-
 slotLeaderHash :: Byron.ABlock ByteString -> ByteString
 slotLeaderHash =
   BS.take 28
     . Crypto.abstractHashToBytes . Crypto.hashRaw .LBS.fromStrict . Crypto.xpubPublicKey
     . Crypto.unVerificationKey . Byron.headerGenesisKey . Byron.blockHeader
 
-slotNumber :: Byron.ABlock ByteString -> Word64
-slotNumber =
-  Byron.unSlotNumber . Byron.headerSlot . Byron.blockHeader
-
 unAbstractHash :: Crypto.Hash Raw -> ByteString
 unAbstractHash = Crypto.abstractHashToBytes
 
 unAddressHash :: Byron.AddressHash Byron.Address' -> ByteString
 unAddressHash = Crypto.abstractHashToBytes
-
-unHeaderHash :: Byron.HeaderHash -> ByteString
-unHeaderHash = Crypto.abstractHashToBytes
-
-unTxHash :: Crypto.Hash Byron.Tx -> ByteString
-unTxHash = Crypto.abstractHashToBytes
 
 unCryptoHash :: Crypto.Hash Raw -> ByteString
 unCryptoHash = Crypto.abstractHashToBytes

.hlint.yaml
@@ -38,6 +38,10 @@
   - {name: [Data.ByteString.Char8], as: BS}
   - {name: [Data.ByteString.Lazy.Char8], as: LBS}
 
+# Banned functions
+- functions:
+  - {name: coerce, within: []}
+
 # Add custom hints for this project
 #
 # Will suggest replacing "wibbleMany [myvar]" with "wibbleOne myvar"

cardano-db-sync/src/Cardano/DbSync/Era/Byron/Genesis.hs
@@ -20,8 +20,7 @@ import qualified Cardano.Binary as Binary
 import qualified Cardano.Chain.Common as Byron
 import qualified Cardano.Chain.Genesis as Byron
 import qualified Cardano.Chain.UTxO as Byron
-import qualified Cardano.Crypto as Crypto (Hash, fromCompactRedeemVerificationKey,
-                   serializeCborHash)
+import qualified Cardano.Crypto as Crypto
 
 import qualified Cardano.Db as DB
 import           Cardano.DbSync.Era.Util (liftLookupFail)
@@ -33,7 +32,6 @@ import           Control.Monad.Trans.Control (MonadBaseControl)
 import           Control.Monad.Trans.Except.Extra (newExceptT)
 
 import qualified Data.ByteString.Char8 as BS
-import           Data.Coerce (coerce)
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
@@ -215,4 +213,7 @@ genesisTxos config =
       Map.toList $ Byron.unGenesisNonAvvmBalances (Byron.configNonAvvmBalances config)
 
 txHashOfAddress :: Byron.Address -> Crypto.Hash Byron.Tx
-txHashOfAddress = coerce . Crypto.serializeCborHash
+txHashOfAddress =
+  fromMaybe (panic "Cardano.DbSync.Era.Byron.Genesis.txHashOfAddress")
+    . Crypto.abstractHashFromBytes . Crypto.abstractHashToBytes . Crypto.serializeCborHash
+

commit e6ca79abea3d6e90084d3ebe10c2eb510d333517

Fix handling of Byron genesis hash
This was inadvertently broken in e28ee284fa90 (not released) when the
usage of `coerce` was removed.

cardano-db-sync/src/Cardano/DbSync/Era/Byron/Insert.hs
@@ -82,8 +82,8 @@ insertABOBBoundary tracer blk details = do
   -- Will not get called in the OBFT part of the Byron era.
   let prevHash = case Byron.boundaryPrevHash (Byron.boundaryHeader blk) of
                     Left gh -> Byron.genesisToHeaderHash gh
-                    Right hh -> hh
-  pbid <- liftLookupFail "insertABOBBoundary" $ DB.queryBlockId (Byron.unHeaderHash prevHash)
+                    Right hh -> Byron.unHeaderHash hh
+  pbid <- liftLookupFail "insertABOBBoundary" $ DB.queryBlockId prevHash
   slid <- lift . DB.insertSlotLeader $
                   DB.SlotLeader
                     { DB.slotLeaderHash = BS.replicate 28 '\0'

cardano-sync/src/Cardano/Sync/Era/Byron/Util.hs
@@ -70,10 +70,8 @@ epochNumber :: Byron.ABlock ByteString -> Word64 -> Word64
 epochNumber blk slotsPerEpoch =
   slotNumber blk `div` slotsPerEpoch
 
-genesisToHeaderHash :: Byron.GenesisHash -> Byron.HeaderHash
-genesisToHeaderHash =
-  fromMaybe (panic "Cardano.Sync.Era.Byron.Util.genesisToHeaderHash")
-    . Crypto.abstractHashFromBytes . Crypto.abstractHashToBytes . Crypto.serializeCborHash
+genesisToHeaderHash :: Byron.GenesisHash -> ByteString
+genesisToHeaderHash = unAbstractHash . Byron.unGenesisHash
 
 protocolVersion :: Byron.ABlock ByteString -> Byron.ProtocolVersion
 protocolVersion = Byron.headerProtocolVersion . Byron.blockHeader