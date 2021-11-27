5a56bf3c4b7d3ef49fee9b838cf8cb1d912f96c0
kderme <kostas.dermentzis@iohk.io>
2021-11-27 09:11:52 +0200

    chain-gen: Add utilities to create Transactions

--- cardano-chain-gen/test/Test/Cardano/Db/Mock/Examples.hs
+++ cardano-chain-gen/test/Test/Cardano/Db/Mock/Examples.hs
@@ -1,13 +1,5 @@
 module Test.Cardano.Db.Mock.Examples where
 
-import           Cardano.Ledger.Alonzo.Tx
-import           Cardano.Ledger.BaseTypes
-import           Cardano.Ledger.Coin
-import           Cardano.Ledger.Shelley.TxBody hiding (TxBody)
-import           Cardano.Ledger.ShelleyMA.Timelocks
-
-import           Ouroboros.Consensus.Shelley.Eras (AlonzoEra, StandardCrypto)
-
 import           Cardano.Mock.Forging.Interpreter
 
 mockBlock0 :: MockBlock
@@ -27,35 +19,3 @@ mockBlock2 = MockBlock
   { txs = []
   , node = NodeId 2
   }
-
-mockBlock3 :: MockBlock
-mockBlock3 = MockBlock
-  { txs = [emptyTx]
-  , node = NodeId 0
-  }
-
-emptyTx :: ValidatedTx (AlonzoEra StandardCrypto)
-emptyTx = ValidatedTx
-  { body = emptyTxBody
-  , wits = mempty
-  , isValid = IsValid True
-  , auxiliaryData = maybeToStrictMaybe Nothing
-  }
-
-emptyTxBody :: TxBody (AlonzoEra StandardCrypto)
-emptyTxBody = TxBody
-  mempty
-  mempty
-  mempty
-  mempty
-  (Wdrl mempty)
-  (Coin 0)
-  (ValidityInterval (maybeToStrictMaybe Nothing) (maybeToStrictMaybe Nothing))
-  (maybeToStrictMaybe Nothing)
-  mempty
-  mempty
-  (maybeToStrictMaybe Nothing)
-  (maybeToStrictMaybe Nothing)
-  (maybeToStrictMaybe $ Just $ Testnet)
-
-
