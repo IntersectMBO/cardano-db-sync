a7680746857ff4363ff96cc39934e771b0981fc6
Erik de Castro Lopo <erikd@mega-nerd.com>
2022-09-28 10:38:53 +1000

    Add test/debug executable http-get-json-metadata
    Can be run in two ways:
    ```
    cabal run -- http-get-json-metadata <url>
    cabal run -- http-get-json-metadata <url> <hash>
    ```
    The first of the above two commands does not check the correctness of
    the metadata hash (eg as specified in the `PoolMetadataRef` table).
    The second of the above two commands does check the hash. If the user
    does not know the hash, they can provide a dummy hash like "00". The
    hash check will then fail, providing the actual hash. If this actual
    hash is then supplied again it should pass.
    
    On success the program will print "Success" and the JSON. On failure it
    prints the error.

---
+++ cardano-db-sync/app/http-get-json-metadata.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE OverloadedStrings #-}
+import           Cardano.Db (PoolMetaHash (..), PoolUrl (..))
+
+import           Cardano.DbSync (FetchError (..), SimplifiedPoolOfflineData (..),
+                   httpGetPoolOfflineData, parsePoolUrl, renderFetchError)
+
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.Trans.Except (ExceptT)
+import           Control.Monad.Trans.Except.Exit (orDie)
+
+import qualified Data.ByteString.Base16 as Base16
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+
+import qualified Network.HTTP.Client as Http
+import           Network.HTTP.Client.TLS (tlsManagerSettings)
+
+import           System.Console.ANSI (setSGRCode)
+import           System.Console.ANSI.Types (Color (..), ColorIntensity (..), ConsoleLayer (..),
+                   SGR (..))
+
+import           System.Environment (getArgs, getProgName)
+import           System.Exit (exitFailure)
+
+main :: IO ()
+main = do
+    xs <- getArgs
+    case xs of
+      [url] -> runHttpGet (PoolUrl $ Text.pack url) Nothing
+      [url, hash] -> runHttpGet (PoolUrl $ Text.pack url) (Just $ parseHash hash)
+      _otherwise -> usageExit
+  where
+    parseHash :: String -> PoolMetaHash
+    parseHash str =
+      case Base16.decode $ BS.pack str of
+        Left err -> error $ "Failed to Base16 decode hash: " ++ err
+        Right bs -> PoolMetaHash bs
+
+-- -------------------------------------------------------------------------------------------------
+
+usageExit :: IO ()
+usageExit = do
+  name <- getProgName
+  mapM_ putStrLn
+    [  "\nUsage:"
+    , "    " ++ name ++ "<pool metadata utl>"
+    , "    " ++ name ++ "<pool metadata utl> <metadata hash in hex>"
+    , ""
+    , "A debug/test program to debug the pool offline metadata fetch mechanism.\n"
+    ]
+  exitFailure
+
+-- -------------------------------------------------------------------------------------------------
+
+runHttpGet :: PoolUrl -> Maybe PoolMetaHash -> IO ()
+runHttpGet poolUrl mHash =
+    reportSuccess =<< orDie renderFetchError httpGet
+  where
+    httpGet :: ExceptT FetchError IO SimplifiedPoolOfflineData
+    httpGet = do
+      request <- parsePoolUrl poolUrl
+      manager <- liftIO $ Http.newManager tlsManagerSettings
+      httpGetPoolOfflineData manager request poolUrl mHash
+
+    reportSuccess :: SimplifiedPoolOfflineData -> IO ()
+    reportSuccess spod = do
+      case spodContentType spod of
+        Nothing -> putStrLn $ orangeText "Warning: No HTTP content-type returned in HTTP response (this should be fixed)."
+        Just ct ->
+          if "application/json" `BS.isInfixOf` ct
+            then putStrLn $ greenText "Success"
+            else putStrLn $  orangeText ("Warning: This should be 'application/json'\nContent-type: " ++ BS.unpack ct)
+      Text.putStrLn $ spodJson spod
+
+-- ------------------------------------------------------------------------------------------------
+
+codeGreen :: String
+codeGreen = setSGRCode [SetColor Foreground Vivid Green]
+
+codeYellow :: String
+codeYellow = setSGRCode [SetColor Foreground Vivid Yellow]
+
+codeReset :: String
+codeReset = setSGRCode [Reset]
+
+greenText :: String -> String
+greenText s = codeGreen ++ s ++ codeReset
+
+orangeText :: String -> String
+orangeText s = codeYellow ++ s ++ codeReset
+
