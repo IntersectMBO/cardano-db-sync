147b25e8b0ce0bf77663474702c5c3db344a0302
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2022-11-07 12:18:07 +0200

    Make it possible to rollback without Reverse Index
    The index can be recreated by queris

--- cardano-db/src/Cardano/Db/Delete.hs
+++ cardano-db/src/Cardano/Db/Delete.hs
@@ -7,6 +7,7 @@
 
 module Cardano.Db.Delete
   ( deleteBlocksSlotNo
+  , deleteBlocksSlotNoNoTrace
   , deleteDelistedPool
   , deleteBlocksBlockId
   , deleteBlock
@@ -15,40 +16,79 @@ module Cardano.Db.Delete
 import           Cardano.Slotting.Slot (SlotNo (..))
 
 import           Control.Monad.Extra (whenJust)
-import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Control.Monad.Trans.Reader (ReaderT)
+
+import           Cardano.BM.Trace (Trace, logWarning, nullTracer)
 
 import           Database.Esqueleto.Experimental (PersistEntity, PersistField, persistIdField)
 import           Database.Persist.Sql (PersistEntityBackend, SqlBackend, delete, selectKeysList, (==.), (>=.))
 import           Database.Persist.Class.PersistQuery (deleteWhere)
 import           Data.ByteString (ByteString)
+import           Data.Maybe (isJust)
+import           Data.Text (Text)
 
 import           Cardano.Db.MinId
-import           Cardano.Db.Query
+import           Cardano.Db.Query hiding (isJust)
 import           Cardano.Db.Schema
 import           Cardano.Db.Text
 
+deleteBlocksSlotNoNoTrace :: MonadIO m => SlotNo -> ReaderT SqlBackend m Bool
+deleteBlocksSlotNoNoTrace = deleteBlocksSlotNo nullTracer
+
 -- | Delete a block if it exists. Returns 'True' if it did exist and has been
 -- deleted and 'False' if it did not exist.
-deleteBlocksSlotNo :: MonadIO m => SlotNo -> ReaderT SqlBackend m Bool
-deleteBlocksSlotNo (SlotNo slotNo) = do
+deleteBlocksSlotNo :: MonadIO m => Trace IO Text -> SlotNo -> ReaderT SqlBackend m Bool
+deleteBlocksSlotNo trce (SlotNo slotNo) = do
   mBlockId <- queryBlockSlotNo slotNo
   case mBlockId of
     Nothing -> pure False
     Just blockId -> do
-      mDel <- deleteBlocksBlockId blockId
-      pure $ either (const False) (const True) mDel
+      deleteBlocksBlockId trce blockId
+      pure True
 
-deleteBlocksBlockId :: MonadIO m => BlockId -> (ReaderT SqlBackend m) (Either LookupFail ())
-deleteBlocksBlockId blockId = do
-    mRollBackId <- queryReverseIndexBlockId blockId
-    case mRollBackId of
-      Nothing -> pure $ Left $ DbLookupMessage $ "Failed to find RollbackIndex for " <> textShow blockId
-      Just rlbId -> do
-        infos <- fmap textToMinId <$> queryMinIdsAfterReverseIndex rlbId
-        let minIds = mconcat infos
-        deleteTablesAfterBlockId blockId (minTxId minIds) (minTxInId minIds) (minTxOutId minIds) (minMaTxOutId minIds)
-        pure $ Right ()
+-- | Delete starting from a 'BlockId'.
+deleteBlocksBlockId :: MonadIO m => Trace IO Text -> BlockId -> ReaderT SqlBackend m ()
+deleteBlocksBlockId trce blockId = do
+    blockIds <- queryBlocksAfter blockId
+    (cminIds, completed) <- findMinIdsRec blockIds mempty
+    minIds <- if completed then pure cminIds else completeMinId blockId cminIds
+    deleteTablesAfterBlockId blockId (minTxId minIds) (minTxInId minIds) (minTxOutId minIds) (minMaTxOutId minIds)
+
+  where
+    findMinIdsRec :: MonadIO m => [BlockId] -> MinIds -> ReaderT SqlBackend m (MinIds, Bool)
+    findMinIdsRec [] minIds = pure (minIds, True)
+    findMinIdsRec (blkId : rest) minIds = do
+      mMinIds <- fmap textToMinId <$> queryReverseIndexBlockId blkId
+      case mMinIds of
+        Nothing -> do
+          liftIO $ logWarning trce $
+            mconcat
+              [ "Failed to find ReverseInex for "
+              , textShow (unBlockId blkId)
+              , ". Deletion may take longer."
+              ]
+          pure (minIds, False)
+        Just minIdDB -> do
+          let minIds' = minIds <> minIdDB
+          if isComplete minIds'
+          then pure (minIds', True)
+          else findMinIdsRec rest minIds'
+
+    isComplete (MinIds m1 m2 m3 m4) = and [isJust m1, isJust m2, isJust m3, isJust m4]
+
+completeMinId :: MonadIO m => BlockId -> MinIds -> ReaderT SqlBackend m MinIds
+completeMinId blkId minIds = do
+    mTxId <- queryMinRefId TxBlockId blkId
+    case mTxId of
+      Nothing -> pure mempty
+      Just txId -> do
+        mTxInId <- whenNothingQueryMinRefId (minTxInId minIds) TxInTxInId txId
+        mTxOutId <- whenNothingQueryMinRefId (minTxOutId minIds) TxOutTxId txId
+        mMaTxOutId <- case mTxOutId of
+          Nothing -> pure Nothing
+          Just txOutId -> whenNothingQueryMinRefId (minMaTxOutId minIds) MaTxOutTxOutId txOutId
+        pure $ MinIds (Just txId) mTxInId mTxOutId mMaTxOutId
 
 deleteTablesAfterBlockId :: MonadIO m => BlockId -> Maybe TxId -> Maybe TxInId -> Maybe TxOutId -> Maybe MaTxOutId -> ReaderT SqlBackend m ()
 deleteTablesAfterBlockId blkId mtxId mtxInId mtxOutId mmaTxOutId = do
@@ -112,6 +152,17 @@ deleteDelistedPool poolHash = do
   mapM_ delete keys
   pure $ not (null keys)
 
+whenNothingQueryMinRefId ::
+     forall m record field. (MonadIO m, PersistEntity record, PersistField field)
+  => Maybe (Key record)
+  -> EntityField record field
+  -> field
+  -> ReaderT SqlBackend m (Maybe (Key record))
+whenNothingQueryMinRefId mKey efield field = do
+    case mKey of
+      Just k -> pure $ Just k
+      Nothing -> queryMinRefId efield field
+
 -- | Delete a block if it exists. Returns 'True' if it did exist and has been
 -- deleted and 'False' if it did not exist.
 deleteBlock :: MonadIO m => Block -> ReaderT SqlBackend m Bool
@@ -120,5 +171,5 @@ deleteBlock block = do
   case mBlockId of
     Nothing -> pure False
     Just blockId -> do
-      mDel <- deleteBlocksBlockId blockId
-      pure $ either (const False) (const True) mDel
+      deleteBlocksBlockId nullTracer blockId
+      pure True
