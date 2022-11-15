6e9f7d310ff1ce8501f407ed5c6f532a03bc24ae
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2022-11-15 13:03:29 +0200

    Add empty migration file
    This will be used to validate fixes of the new release

---
+++ schema/migration-2-0020-20220919.sql
@@ -0,0 +1,20 @@
+-- Persistent generated migration.
+
+CREATE FUNCTION migrate() RETURNS void AS $$
+DECLARE
+  next_version int ;
+BEGIN
+  SELECT stage_two + 1 INTO next_version FROM schema_version ;
+  IF next_version = 20 THEN
+    -- Empty migration file just to raise the version
+    -- This indicates that the fixing operation has occured
+
+    UPDATE schema_version SET stage_two = next_version ;
+    RAISE NOTICE 'DB has been migrated to stage_two version %', next_version ;
+  END IF ;
+END ;
+$$ LANGUAGE plpgsql ;
+
+SELECT migrate() ;
+
+DROP FUNCTION migrate() ;
