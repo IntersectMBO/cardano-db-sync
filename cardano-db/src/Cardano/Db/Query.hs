99a8117efe11730e8b7fe7c2ac179f6554c92f80
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2022-12-02 15:53:24 +0200

    Fix bug with rollbacks

--- cardano-db/src/Cardano/Db/Query.hs
+++ cardano-db/src/Cardano/Db/Query.hs
@@ -541,7 +541,7 @@ queryMinRefId txIdField txId = do
   res <- select $ do
     rec <- from $ table @record
     where_ (rec ^. txIdField >=. val txId)
-    orderBy [asc (rec ^. txIdField)]
+    orderBy [asc (rec ^. persistIdField)]
     limit 1
     pure $ rec ^. persistIdField
   pure $ unValue <$> listToMaybe res
