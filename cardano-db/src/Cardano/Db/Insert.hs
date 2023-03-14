5a4bef116b0c946a6b1c385eaa20e20a5dd4711a
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2023-03-14 17:10:33 +0200

    Insert TxMetadata in bulk per block
    Instead of inseting them one by one

--- cardano-db/src/Cardano/Db/Insert.hs
+++ cardano-db/src/Cardano/Db/Insert.hs
@@ -39,7 +39,7 @@ module Cardano.Db.Insert (
   insertTreasury,
   insertTx,
   insertTxIn,
-  insertTxMetadata,
+  insertManyTxMetadata,
   insertTxOut,
   insertCollateralTxOut,
   insertManyTxOut,
@@ -219,8 +219,8 @@ insertTx tx = insertUnchecked ("Tx: " ++ show (BS.length (txHash tx))) tx
 insertTxIn :: (MonadBaseControl IO m, MonadIO m) => TxIn -> ReaderT SqlBackend m TxInId
 insertTxIn = insertUnchecked "TxIn"
 
-insertTxMetadata :: (MonadBaseControl IO m, MonadIO m) => TxMetadata -> ReaderT SqlBackend m TxMetadataId
-insertTxMetadata = insertUnchecked "TxMetadata"
+insertManyTxMetadata :: (MonadBaseControl IO m, MonadIO m) => [TxMetadata] -> ReaderT SqlBackend m [TxMetadataId]
+insertManyTxMetadata = insertMany' "TxMetadata"
 
 insertTxOut :: (MonadBaseControl IO m, MonadIO m) => TxOut -> ReaderT SqlBackend m TxOutId
 insertTxOut = insertUnchecked "TxOut"
