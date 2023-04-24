21b458527a18417b9584f61b3b4b7253d6f330ee
Cmdv <vincent@cmdv.me>
2023-04-24 14:36:04 +0100

    make correct checks for handling epoch cache on rollbacks

--- cardano-db-sync/src/Cardano/DbSync/Cache/Epoch.hs
+++ cardano-db-sync/src/Cardano/DbSync/Cache/Epoch.hs
@@ -8,8 +8,9 @@ module Cardano.DbSync.Cache.Epoch (
 
 import qualified Cardano.Db as DB
 import Cardano.DbSync.Cache.Types (Cache (..), CacheEpoch (..), CacheInternal (..))
-import Cardano.DbSync.Types (CardanoBlock)
+import Cardano.DbSync.Types (CardanoBlock, EpochSlot (..))
 import Cardano.Prelude
+import Cardano.Slotting.Slot (EpochNo (..))
 import Control.Concurrent.Class.MonadSTM.Strict (readTVarIO, writeTVar)
 import Database.Persist.Postgresql (SqlBackend)
 
@@ -47,19 +48,31 @@ writeBlockAndFeeToCacheEpoch ::
   Cache ->
   CardanoBlock ->
   Word64 ->
+  EpochNo ->
   ReaderT SqlBackend m ()
-writeBlockAndFeeToCacheEpoch cache block fees =
+writeBlockAndFeeToCacheEpoch cache block fees blockEpochNo =
   case cache of
     UninitiatedCache -> pure ()
     Cache ci -> do
       cachedEpoch <- liftIO $ readTVarIO (cEpoch ci)
       case cachedEpoch of
         Nothing -> do
-          -- If we don't have an CacheEpoch then this is either the first block
-          -- or we're syncing for the first time and we need to continue where we left off
+          -- If we don't have an CacheEpoch then this is either the first block (Syncing/Folowing)
+          -- So let's try and attempt to get the latest epoch from DB.
           latestEpochFromDb <- DB.queryLatestEpoch
-          liftIO $ atomically $ writeTVar (cEpoch ci) (Just $ CacheEpoch latestEpochFromDb block fees)
+          case latestEpochFromDb of
+            -- We don't have any epochs on the DB do this is the first block
+            Nothing -> writeToCacheWithEpoch ci Nothing
+            -- An Epoch is returned but we need to make sure it's in the same epoch as the current block.
+            -- If we're wanting to add it to cache.
+            Just ep ->
+              if DB.epochNo ep == unEpochNo blockEpochNo
+                then writeToCacheWithEpoch ci (Just ep)
+                else writeToCacheWithEpoch ci Nothing
         Just cacheE -> liftIO $ atomically $ writeTVar (cEpoch ci) (Just cacheE {ceBlock = block, ceFees = fees})
+  where
+    writeToCacheWithEpoch ci latestEpochFromDb =
+      liftIO $ atomically $ writeTVar (cEpoch ci) (Just $ CacheEpoch latestEpochFromDb block fees)
 
 writeEpochToCacheEpoch ::
   MonadIO m =>
