1dc84fd5f91da68cfed249ad79a465abc61f8e9a
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2023-07-21 09:27:03 +0300

    Introduce Conway block and simplify getTx

--- cardano-db-sync/src/Cardano/DbSync/Era/Shelley/Generic/Tx.hs
+++ cardano-db-sync/src/Cardano/DbSync/Era/Shelley/Generic/Tx.hs
@@ -4,12 +4,14 @@ module Cardano.DbSync.Era.Shelley.Generic.Tx (
   fromMaryTx,
   fromAlonzoTx,
   fromBabbageTx,
+  fromConwayTx,
   module X,
 ) where
 
 import Cardano.DbSync.Era.Shelley.Generic.Tx.Allegra
 import Cardano.DbSync.Era.Shelley.Generic.Tx.Alonzo
 import Cardano.DbSync.Era.Shelley.Generic.Tx.Babbage
+import Cardano.DbSync.Era.Shelley.Generic.Tx.Conway
 import Cardano.DbSync.Era.Shelley.Generic.Tx.Mary
 import Cardano.DbSync.Era.Shelley.Generic.Tx.Shelley
 import Cardano.DbSync.Era.Shelley.Generic.Tx.Types as X
