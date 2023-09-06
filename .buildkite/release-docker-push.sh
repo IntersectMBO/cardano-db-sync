a0bb11d77209bdb38ee0cbbe10acc920b0381f32
Sean D Gillespie <sean.gillespie@iohk.io>
2023-09-06 08:48:39 -0400

    build: Remove Buildkite jobs

--- .buildkite/release-docker-push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! { [ "${BUILDKITE_BRANCH:-}" == "master" ] || [[ "${BUILDKITE_BRANCH:-}" = release* ]]; }; then
-  exit 0;
-else
-  nix-build .buildkite/docker-build-push.nix --argstr dockerHubRepoName inputoutput/cardano-db-sync -o docker-build-push
-  ./docker-build-push
-fi
