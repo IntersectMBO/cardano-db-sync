7e89b59ddc26e66783f9dad7e76cb29d860f61f8
Sean D Gillespie <sean.gillespie@iohk.io>
2023-10-03 16:11:42 -0400

    test(cardano-chain-gen): Add Conway "simple" tests

--- cardano-chain-gen/test/Test/Cardano/Db/Mock/Unit/Conway.hs
+++ cardano-chain-gen/test/Test/Cardano/Db/Mock/Unit/Conway.hs
@@ -3,12 +3,13 @@ module Test.Cardano.Db.Mock.Unit.Conway (unitTests) where
 import Cardano.Mock.ChainSync.Server (IOManager ())
 import Cardano.Prelude
 import qualified Test.Cardano.Db.Mock.Unit.Conway.Config as ConConfig
+import qualified Test.Cardano.Db.Mock.Unit.Conway.Simple as Simple
 import Test.Tasty (TestTree (), testGroup)
-import Test.Tasty.HUnit (testCase)
-import Prelude ()
+import Test.Tasty.HUnit (Assertion (), testCase)
+import Prelude (String ())
 
 unitTests :: IOManager -> [(Text, Text)] -> TestTree
-unitTests _ _ =
+unitTests iom knownMigrations =
   testGroup
     "Conway unit tests"
     [ testGroup
@@ -19,4 +20,16 @@ unitTests _ _ =
         , testCase "no conway genesis hash" ConConfig.noConwayGenesisHash
         , testCase "mismatched conway genesis hash" ConConfig.wrongConwayGenesisHash
         ]
+    , testGroup
+        "simple"
+        [ test "simple forge blocks" Simple.forgeBlocks
+        , test "sync one block" Simple.addSimple
+        , test "sync small chain" Simple.addSimpleChain
+        , test "restart db-sync" Simple.restartDBSync
+        , test "node restart" Simple.nodeRestart
+        , test "node restart boundary" Simple.nodeRestartBoundary
+        ]
     ]
+  where
+    test :: String -> (IOManager -> [(Text, Text)] -> Assertion) -> TestTree
+    test str action = testCase str (action iom knownMigrations)
