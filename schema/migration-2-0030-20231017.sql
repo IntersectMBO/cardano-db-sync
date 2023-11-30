8c03df149071da6f2f56a49db86fd913c5558153
sorki <srk@48.io>
2023-11-30 07:38:54 +0100

    schema: ALTER SEQUENCE for pool_offline -> off_chain_pool renaming

--- schema/migration-2-0030-20231017.sql
+++ schema/migration-2-0030-20231017.sql
@@ -7,10 +7,12 @@ BEGIN
   SELECT stage_two + 1 INTO next_version FROM schema_version ;
   IF next_version = 30 THEN
     EXECUTE 'ALTER TABLE "pool_offline_data" RENAME TO "off_chain_pool_data"' ;
+    EXECUTE 'ALTER SEQUENCE "pool_offline_data_id_seq" RENAME TO "off_chain_pool_data_id_seq"' ;
     EXECUTE 'ALTER TABLE "off_chain_pool_data" RENAME CONSTRAINT "unique_pool_offline_data" TO "unique_off_chain_pool_data"' ;
     EXECUTE 'ALTER TABLE "off_chain_pool_data" RENAME CONSTRAINT "pool_offline_data_pkey" TO "off_chain_pool_data_pkey"' ;
 
     EXECUTE 'ALTER TABLE "pool_offline_fetch_error" RENAME TO "off_chain_pool_fetch_error"' ;
+    EXECUTE 'ALTER SEQUENCE "pool_offline_fetch_error_id_seq" RENAME TO "off_chain_pool_fetch_error_id_seq"' ;
     EXECUTE 'ALTER TABLE "off_chain_pool_fetch_error" RENAME CONSTRAINT "unique_pool_offline_fetch_error" TO "unique_off_chain_pool_fetch_error"' ;
     EXECUTE 'ALTER TABLE "off_chain_pool_fetch_error" RENAME CONSTRAINT "pool_offline_fetch_error_pkey" TO "off_chain_pool_fetch_error_pkey"' ;
 
