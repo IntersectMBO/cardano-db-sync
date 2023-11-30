4043d1b9b6011d151a27795efc3b077245fbf891
sorki <srk@48.io>
2023-11-30 07:32:48 +0100

    schema: Fix cold_key -> hot_key migration
    otherwise it fails due to NOT NULL contraint.

--- schema/migration-2-0032-20231107.sql
+++ schema/migration-2-0032-20231107.sql
@@ -6,8 +6,7 @@ DECLARE
 BEGIN
   SELECT stage_two + 1 INTO next_version FROM schema_version ;
   IF next_version = 32 THEN
-    EXECUTE 'ALTER TABLE "committee_de_registration" ADD COLUMN "cold_key" bytea NOT NULL' ;
-    EXECUTE 'ALTER TABLE "committee_de_registration" DROP COLUMN "hot_key"' ;
+    EXECUTE 'ALTER TABLE "committee_de_registration" RENAME COLUMN "hot_key" TO "cold_key"' ;
     EXECUTE 'CREATe TABLE "off_chain_anchor_data"("id" SERIAL8  PRIMARY KEY UNIQUE,"voting_anchor_id" INT8 NOT NULL,"hash" BYTEA NOT NULL,"json" jsonb NOT NULL,"bytes" bytea NOT NULL)' ;
     EXECUTE 'CREATe TABLE "off_chain_anchor_fetch_error"("id" SERIAL8  PRIMARY KEY UNIQUE,"voting_anchor_id" INT8 NOT NULL,"fetch_error" VARCHAR NOT NULL,"retry_count" word31type NOT NULL)' ;
     -- Hand written SQL statements can be added here.
