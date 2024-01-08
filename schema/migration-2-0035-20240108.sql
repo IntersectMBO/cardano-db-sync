e9949cecd31a25b6c4d40f8e772f479fd1bdecbc
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2024-01-08 22:07:32 +0200

    Drop uneeded checks from domains
    Fixes https://github.com/IntersectMBO/cardano-db-sync/issues/1584

---
+++ schema/migration-2-0035-20240108.sql
@@ -0,0 +1,29 @@
+-- Persistent generated migration.
+
+CREATE FUNCTION migrate() RETURNS void AS $$
+DECLARE
+  next_version int ;
+BEGIN
+  SELECT stage_two + 1 INTO next_version FROM schema_version ;
+  IF next_version = 35 THEN
+    EXECUTE 'alter domain addr29type drop constraint addr29type_check' ;
+    EXECUTE 'alter domain asset32type drop constraint asset32type_check' ;
+    EXECUTE 'alter domain hash28type drop constraint hash28type_check' ;
+    EXECUTE 'alter domain hash32type drop constraint hash32type_check' ;
+    EXECUTE 'alter domain int65type drop constraint int65type_check' ;
+    EXECUTE 'alter domain lovelace drop constraint lovelace_check' ;
+    EXECUTE 'alter domain txindex drop constraint txindex_check' ;
+    EXECUTE 'alter domain word128type drop constraint word128type_check' ;
+    EXECUTE 'alter domain word31type drop constraint word31type_check' ;
+    EXECUTE 'alter domain word63type drop constraint word63type_check' ;
+    EXECUTE 'alter domain word64type drop constraint word64type_check' ;
+    -- Hand written SQL statements can be added here.
+    UPDATE schema_version SET stage_two = next_version ;
+    RAISE NOTICE 'DB has been migrated to stage_two version %', next_version ;
+  END IF ;
+END ;
+$$ LANGUAGE plpgsql ;
+
+SELECT migrate() ;
+
+DROP FUNCTION migrate() ;
