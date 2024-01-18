00c2ff37b4767bd9b80d5833c62afe232c322e04
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2024-01-18 11:13:13 +0200

    Add migration to remove redundant tx_out fields

---
+++ schema/migration-2-0036-20240117.sql
@@ -0,0 +1,21 @@
+-- Persistent generated migration.
+
+CREATE FUNCTION migrate() RETURNS void AS $$
+DECLARE
+  next_version int ;
+BEGIN
+  SELECT stage_two + 1 INTO next_version FROM schema_version ;
+  IF next_version = 36 THEN
+    EXECUTE 'ALTER TABLE "tx_out" DROP COLUMN "address_raw"' ;
+
+    EXECUTE 'ALTER TABLE "collateral_tx_out" DROP COLUMN "address_raw"' ;
+    -- Hand written SQL statements can be added here.
+    UPDATE schema_version SET stage_two = next_version ;
+    RAISE NOTICE 'DB has been migrated to stage_two version %', next_version ;
+  END IF ;
+END ;
+$$ LANGUAGE plpgsql ;
+
+SELECT migrate() ;
+
+DROP FUNCTION migrate() ;
