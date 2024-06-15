5daaa294cd80807c33f8b90567acba117d043106
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2024-06-16 00:54:00 +0300

    Remove and add indexes

--- schema/migration-3-0004-20240603.sql
+++ schema/migration-3-0004-20240603.sql
@@ -1,2 +1,3 @@
 
 CREATE INDEX IF NOT EXISTS idx_committee_member_committee_id ON committee_member (committee_id);
+CREATE INDEX IF NOT EXISTS idx_pool_update_registered_tx_id ON pool_update (registered_tx_id);
