868974b861856a69c35299544505ede05f3292f0
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2024-06-16 00:54:00 +0300

    Remove and add indexes

--- schema/migration-4-0004-20210116.sql
+++ schema/migration-4-0004-20210116.sql
@@ -1,6 +1,5 @@
 
 -- Indexes which optimize inserting new rewards
-CREATE INDEX IF NOT EXISTS idx_stake_address_hash_raw ON stake_address(hash_raw) ;
 CREATE INDEX IF NOT EXISTS idx_delegation_active_epoch_no ON delegation(active_epoch_no) ;
 CREATE INDEX IF NOT EXISTS idx_pool_update_reward_addr ON pool_update(reward_addr_id) ;
 CREATE INDEX IF NOT EXISTS idx_pool_update_active_epoch_no ON pool_update(active_epoch_no) ;
