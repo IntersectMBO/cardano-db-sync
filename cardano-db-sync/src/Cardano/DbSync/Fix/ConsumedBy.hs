61d22d37cd86d05b6c3c7d10253b985e9dd724bb
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2024-08-26 16:35:34 +0300

    Do multiple updates in a single transaction

--- cardano-db-sync/src/Cardano/DbSync/Fix/ConsumedBy.hs
+++ cardano-db-sync/src/Cardano/DbSync/Fix/ConsumedBy.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Cardano.DbSync.Fix.ConsumedBy (fixConsumedBy) where
+module Cardano.DbSync.Fix.ConsumedBy (FixEntry, fixConsumedBy, fixEntriesConsumed) where
 
 import Cardano.BM.Trace (Trace, logWarning)
 import qualified Cardano.Chain.Block as Byron hiding (blockHash)
@@ -12,44 +12,45 @@ import Cardano.DbSync.Era.Byron.Util (blockPayload, unTxHash)
 import Cardano.DbSync.Era.Util
 import Cardano.DbSync.Error
 import Cardano.DbSync.Types
-import Cardano.Prelude hiding (length)
+import Cardano.Prelude hiding (length, (.))
 import Database.Persist.SqlBackend.Internal
 import Ouroboros.Consensus.Byron.Ledger (ByronBlock (..))
 import Ouroboros.Consensus.Cardano.Block (HardForkBlock (..))
 
-fixConsumedBy :: SqlBackend -> Trace IO Text -> Integer -> CardanoBlock -> IO (Integer, Bool)
-fixConsumedBy backend tracer lastSize cblk = case cblk of
-  BlockByron blk -> (\(n, bl) -> (n + lastSize, bl)) <$> fixBlock backend tracer blk
-  _ -> pure (lastSize, True)
+type FixEntry = (DB.TxOutId, DB.TxId)
 
-fixBlock :: SqlBackend -> Trace IO Text -> ByronBlock -> IO (Integer, Bool)
+-- | Nothing when the syncing must stop.
+fixConsumedBy :: SqlBackend -> Trace IO Text -> CardanoBlock -> IO (Maybe [FixEntry])
+fixConsumedBy backend tracer cblk = case cblk of
+  BlockByron blk -> fixBlock backend tracer blk
+  _ -> pure Nothing
+
+fixBlock :: SqlBackend -> Trace IO Text -> ByronBlock -> IO (Maybe [FixEntry])
 fixBlock backend tracer bblk = case byronBlockRaw bblk of
-  Byron.ABOBBoundary _ -> pure (0, False)
+  Byron.ABOBBoundary _ -> pure $ Just []
   Byron.ABOBBlock blk -> do
-    runReaderT (fix 0 (blockPayload blk)) backend
-  where
-    fix totalSize [] = pure (totalSize, False)
-    fix totalSize (tx : txs) = do
-      mn <- runExceptT $ fixTx tx
-      case mn of
-        Right n -> fix (totalSize + n) txs
-        Left err -> do
-          liftIO $
-            logWarning tracer $
-              mconcat
-                [ "While fixing tx "
-                , textShow tx
-                , ", encountered error "
-                , textShow err
-                ]
-          pure (totalSize, True)
+    mEntries <- runReaderT (runExceptT $ mapM fixTx (blockPayload blk)) backend
+    case mEntries of
+      Right newEntries -> pure $ Just $ concat newEntries
+      Left err -> do
+        liftIO $
+          logWarning tracer $
+            mconcat
+              [ "While fixing block "
+              , textShow bblk
+              , ", encountered error "
+              , textShow err
+              ]
+        pure Nothing
 
-fixTx :: MonadIO m => Byron.TxAux -> ExceptT SyncNodeError (ReaderT SqlBackend m) Integer
+fixTx :: MonadIO m => Byron.TxAux -> ExceptT SyncNodeError (ReaderT SqlBackend m) [FixEntry]
 fixTx tx = do
   txId <- liftLookupFail "resolving tx" $ DB.queryTxId txHash
   resolvedInputs <- mapM resolveTxInputs (toList $ Byron.txInputs (Byron.taTx tx))
-  lift $ DB.updateListTxOutConsumedByTxId (prepUpdate txId <$> resolvedInputs)
-  pure $ fromIntegral $ length resolvedInputs
+  pure (prepUpdate txId <$> resolvedInputs)
   where
     txHash = unTxHash $ Crypto.serializeCborHash (Byron.taTx tx)
     prepUpdate txId (_, _, txOutId, _) = (txOutId, txId)
+
+fixEntriesConsumed :: SqlBackend -> Trace IO Text -> [FixEntry] -> IO ()
+fixEntriesConsumed backend tracer = DB.runDbIohkLogging backend tracer . DB.updateListTxOutConsumedByTxId
