1690fe1503a9a39ee2b8ded403f0fad2e9f53b02
Cmdv <vincent@cmdv.me>
2025-02-21 11:54:42 +0000

    Add new schema types

---
+++ cardano-db/src/Cardano/Db/Schema/Core.hs
@@ -0,0 +1,17 @@
+module Cardano.Db.Schema.Core
+  ( module Cardano.Db.Schema.Core.Base
+  , module Cardano.Db.Schema.Core.EpochAndProtocol
+  , module Cardano.Db.Schema.Core.GovernanceAndVoting
+  , module Cardano.Db.Schema.Core.MultiAsset
+  , module Cardano.Db.Schema.Core.OffChain
+  , module Cardano.Db.Schema.Core.Pool
+  , module Cardano.Db.Schema.Core.StakeDeligation
+  ) where
+
+import Cardano.Db.Schema.Core.Base
+import Cardano.Db.Schema.Core.EpochAndProtocol
+import Cardano.Db.Schema.Core.GovernanceAndVoting
+import Cardano.Db.Schema.Core.MultiAsset
+import Cardano.Db.Schema.Core.OffChain
+import Cardano.Db.Schema.Core.Pool
+import Cardano.Db.Schema.Core.StakeDeligation
