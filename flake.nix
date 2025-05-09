1e15f8deeb392d04431d4bbb623078f44d0627be
Sean D Gillespie <sean.gillespie@iohk.io>
2025-05-09 13:03:59 -0400

    build(nix): Fix static checks

--- flake.nix
+++ flake.nix
@@ -77,6 +77,15 @@
                   })
 
                 (final: prev: {
+                  fourmolu = final.haskell-nix.tool "ghc96" "fourmolu" {
+                    version = "latest";
+                  };
+
+                  hlint = final.haskell-nix.tool "ghc96" "hlint" {
+                  };
+                })
+
+                (final: prev: {
                   postgresql = prev.postgresql.overrideAttrs (_:
                     final.lib.optionalAttrs (final.stdenv.hostPlatform.isMusl) {
                       NIX_LDFLAGS = "--push-state --as-needed -lstdc++ --pop-state";
