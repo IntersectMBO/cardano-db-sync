a22dfe64e234241a88da3fa5024cae4c7ccd2eb2
Kostas Dermentzis <kostas.dermentzis@iohk.io>
2026-04-21 19:27:52 +0300

    Remove epoch stake entries with 0 amount
    The ledger no longer provides them, so we historically delete them
    for consistency

---
+++ schema/migration-2-0047-20260421.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION migrate() RETURNS void AS $$
+DECLARE
+  next_version int ;
+BEGIN
+  SELECT stage_two + 1 INTO next_version FROM schema_version ;
+  IF next_version = 47 THEN
+
+    DELETE FROM "epoch_stake" WHERE "amount" = 0 ;
+
+    UPDATE schema_version SET stage_two = next_version ;
+    RAISE NOTICE 'DB has been migrated to stage_two version %', next_version ;
+  END IF ;
+END ;
+$$ LANGUAGE plpgsql ;
+
+SELECT migrate() ;
+DROP FUNCTION migrate() ;
