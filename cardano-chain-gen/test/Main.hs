99922693c48c01cce1f263fe1f7318cccff8a9b2
Sean Gillespie <sean@mistersg.net>
2026-06-29 16:49:59 -0400

    Remove hardcoded `NumThreads 1` from DB test
    This allows the user to add `-j N` to control how many threads to run

--- cardano-chain-gen/test/Main.hs
+++ cardano-chain-gen/test/Main.hs
@@ -17,7 +17,6 @@ import qualified Test.Cardano.Db.Mock.Unit.Babbage as Babbage
 import qualified Test.Cardano.Db.Mock.Unit.Conway as Conway
 import Test.Tasty
 import Test.Tasty.QuickCheck (testProperty)
-import Test.Tasty.Runners (NumThreads (..))
 import Prelude
 
 main :: IO ()
@@ -45,19 +44,18 @@ tests iom = do
     pgCfg4 = pgCfg {pgcDbname = pgcDbname <> "_4"}
 
   pure $
-    localOption (NumThreads 1) $
-      testGroup
-        "all"
-        [ -- Tests share a single Postgres instance and DB, so they must run serially.
-          -- tasty 1.5+ defaults to parallel execution; override that here.
-          testGroup
-            "cardano-chain-gen"
-            [ Conway.unitTests iom knownMigrationsPlain (PGPassCached pgCfg1)
-            , Babbage.unitTests iom knownMigrationsPlain (PGPassCached pgCfg2)
-            , Alonzo.unitTests iom knownMigrationsPlain (PGPassCached pgCfg3)
-            , testProperty "QSM" $ Property.prop_empty_blocks (PGPassCached pgCfg4) iom knownMigrationsPlain
-            ]
-        ]
+    testGroup
+      "all"
+      [ -- Tests share a single Postgres instance and DB, so they must run serially.
+        -- tasty 1.5+ defaults to parallel execution; override that here.
+        testGroup
+          "cardano-chain-gen"
+          [ Conway.unitTests iom knownMigrationsPlain (PGPassCached pgCfg1)
+          , Babbage.unitTests iom knownMigrationsPlain (PGPassCached pgCfg2)
+          , Alonzo.unitTests iom knownMigrationsPlain (PGPassCached pgCfg3)
+          , testProperty "QSM" $ Property.prop_empty_blocks (PGPassCached pgCfg4) iom knownMigrationsPlain
+          ]
+      ]
   where
     knownMigrationsPlain :: [(Text, Text)]
     knownMigrationsPlain = (\x -> (hash x, filepath x)) <$> knownMigrations
